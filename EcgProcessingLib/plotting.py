73a43137a1ad918e703438481f4c46fefa480cc9
Robert Richer <robert.richer@fau.de>
2020-06-12 08:47:30 +0200

    plot improvements

--- EcgProcessingLib/plotting.py
+++ EcgProcessingLib/plotting.py
@@ -23,10 +23,11 @@ mpl_rc_params = {
 plt.rcParams.update(mpl_rc_params)
 
 
+# TODO add kwargs
 def ecg_plot(ecg_signals: pd.DataFrame, heart_rate: pd.DataFrame, sampling_rate: Optional[int] = 256,
-             name: Optional[str] = None, plot_histogram: Optional[bool] = False,
+             name: Optional[str] = None, plot_distribution: Optional[bool] = False,
              plot_individual_beats: Optional[bool] = False) -> plt.Figure:
-    import matplotlib.gridspec
+    import matplotlib.gridspec as gs
     import matplotlib.dates as mdates
     import matplotlib.ticker as mticks
 
@@ -41,25 +42,25 @@ def ecg_plot(ecg_signals: pd.DataFrame, heart_rate: pd.DataFrame, sampling_rate:
 
     fig = plt.figure(figsize=(15, 5), constrained_layout=False)
 
-    if plot_individual_beats or plot_histogram:
-        gs = matplotlib.gridspec.GridSpec(2, 2, width_ratios=[3 / 4, 1 / 4])
+    if plot_individual_beats or plot_distribution:
+        spec = gs.GridSpec(2, 2, width_ratios=[3, 1])
         axs = {
-            'ecg': fig.add_subplot(gs[0, :-1]),
-            'hr': fig.add_subplot(gs[1, :-1])
+            'ecg': fig.add_subplot(spec[0, :-1]),
+            'hr': fig.add_subplot(spec[1, :-1])
         }
-        if plot_histogram and plot_individual_beats:
-            axs['hist'] = fig.add_subplot(gs[0, -1])
-            axs['beats'] = fig.add_subplot(gs[1, -1])
+        if plot_distribution and plot_individual_beats:
+            axs['dist'] = fig.add_subplot(spec[0, -1])
+            axs['beats'] = fig.add_subplot(spec[1, -1])
         elif plot_individual_beats:
-            axs['beats'] = fig.add_subplot(gs[:, -1])
-        elif plot_histogram:
-            axs['hist'] = fig.add_subplot(gs[:, -1])
-
+            axs['beats'] = fig.add_subplot(spec[:, -1])
+        elif plot_distribution:
+            axs['dist'] = fig.add_subplot(spec[:, -1])
     else:
         axs = {
             'ecg': fig.add_subplot(2, 1, 1),
             'hr': fig.add_subplot(2, 1, 2)
         }
+
     axs['ecg'].get_shared_x_axes().join(axs['ecg'], axs['hr'])
 
     if name:
@@ -102,8 +103,8 @@ def ecg_plot(ecg_signals: pd.DataFrame, heart_rate: pd.DataFrame, sampling_rate:
     if plot_individual_beats:
         individual_beats_plot(ecg_signals, peaks, sampling_rate, axs['beats'])
 
-    if plot_histogram:
-        ecg_histogram_plot(heart_rate, axs['hist'])
+    if plot_distribution:
+        ecg_distribution_plot(heart_rate, axs['hist'])
 
     fig.tight_layout()
     fig.autofmt_xdate(rotation=0, ha='center')
@@ -117,9 +118,9 @@ def ecg_plot(ecg_signals: pd.DataFrame, heart_rate: pd.DataFrame, sampling_rate:
     if plot_individual_beats:
         axs['beats'].tick_params(axis='x', which='major', bottom=True, labelbottom=True)
 
-    if plot_histogram:
-        axs['hist'].tick_params(axis='x', which='major', bottom=True, labelbottom=True)
-        axs['hist'].set_xlabel("Heart Rate [bpm]")
+    if plot_distribution:
+        axs['dist'].tick_params(axis='x', which='major', bottom=True, labelbottom=True)
+        axs['dist'].set_xlabel("Heart Rate [bpm]")
 
     return fig
 
@@ -160,16 +161,43 @@ def hr_plot(ecg_signals: pd.DataFrame, ax: Optional[plt.Axes] = None,
     return ax
 
 
-def individual_beats_plot(df_ecg: pd.DataFrame, peaks: Optional[Sequence[int]] = None,
-                          sampling_rate: Optional[int] = 256.0, ax: Optional[plt.Axes] = None):
+def hrv_plot(rpeaks: pd.DataFrame, sampling_rate: Optional[int] = 256, plot_frequency: Optional[bool] = True):
+    import matplotlib.gridspec as gs
+    fig = plt.figure(constrained_layout=False, figsize=(12, 7))
+
+    if plot_frequency:
+        spec = gs.GridSpec(ncols=2, nrows=2, height_ratios=[1, 1], width_ratios=[1, 1])
+        axs = {
+            'dist': fig.add_subplot(spec[0, :-1]),
+            'freq': fig.add_subplot(spec[1, :-1]),
+        }
+    else:
+        spec = gs.GridSpec(ncols=2, nrows=1, width_ratios=[1, 1])
+        axs = {
+            'dist': fig.add_subplot(spec[0, :-1])
+        }
+
+    spec_within = gs.GridSpecFromSubplotSpec(4, 4, subplot_spec=spec[:, -1], wspace=0.025, hspace=0.05)
+    axs['poin'] = fig.add_subplot(spec_within[1:4, 0:3])
+    axs['poin_x'] = fig.add_subplot(spec_within[0, 0:3])
+    # axs['poin_x'].set_title("Poincaré Plot")
+    axs['poin_y'] = fig.add_subplot(spec_within[1:4, 3])
+
+    axs['dist'] = hrv_distribution_plot(rpeaks, sampling_rate, axs['dist'])
+
+    fig.tight_layout()
+
+
+def individual_beats_plot(ecg_signals: pd.DataFrame, peaks: Optional[Sequence[int]] = None,
+                          sampling_rate: Optional[int] = 256, ax: Optional[plt.Axes] = None):
     fig: Union[plt.Figure, None] = None
     if ax is None:
         fig, ax = plt.subplots()
 
     if peaks is None:
-        peaks = np.where(df_ecg["ECG_R_Peaks"] == 1)[0]
+        peaks = np.where(ecg_signals["ECG_R_Peaks"] == 1)[0]
 
-    heartbeats = nk.ecg_segment(df_ecg['ECG_Clean'], peaks, sampling_rate)
+    heartbeats = nk.ecg_segment(ecg_signals['ECG_Clean'], peaks, sampling_rate)
     heartbeats = nk.epochs_to_df(heartbeats)
     heartbeats_pivoted = heartbeats.pivot(index='Time', columns='Label', values='Signal')
 
@@ -189,14 +217,14 @@ def individual_beats_plot(df_ecg: pd.DataFrame, peaks: Optional[Sequence[int]] =
         return ax
 
 
-def ecg_histogram_plot(heart_rate: pd.DataFrame, ax: Optional[plt.Axes] = None):
+def ecg_distribution_plot(heart_rate: pd.DataFrame, ax: Optional[plt.Axes] = None):
     fig: Union[plt.Figure, None] = None
     if ax is None:
         fig, ax = plt.subplots()
 
-    sns.distplot(heart_rate, color=utils.fau_color('tech'), ax=ax)
+    ax = sns.distplot(heart_rate, color=utils.fau_color('tech'), ax=ax)
 
-    ax.set_title("Histogram")
+    ax.set_title("Heart Rate Distribution")
     ax.set_xlabel("Heart Rate [bpm]")
     ax.set_yticks([])
     ax.set_xlim(heart_rate.min().min() - 1, heart_rate.max().max() + 1)
@@ -211,3 +239,41 @@ def ecg_plot_artifacts(ecg_signals: pd.DataFrame, sampling_rate: Optional[int] =
     # Plot artifacts
     _, rpeaks = nk.ecg_peaks(ecg_signals["ECG_Clean"], sampling_rate=sampling_rate)
     _, _ = nk.ecg_fixpeaks(rpeaks, sampling_rate=sampling_rate, iterative=True, show=True)
+
+
+def hrv_distribution_plot(rpeaks: pd.DataFrame, sampling_rate: Optional[int] = 256,
+                          ax: Optional[plt.Axes] = None) -> plt.Axes:
+    rri: np.ndarray = (np.ediff1d(rpeaks['R_Peak_Idx'], to_begin=0) / sampling_rate) * 1000
+    rri = rri[1:]
+
+    rug_height = 0.05
+
+    sns.distplot(rri, color=utils.fau_color('med'), ax=ax, bins=10, kde=False, rug=True,
+                 rug_kws={'color': utils.fau_color('fau'), 'lw': 1.5, 'height': rug_height, 'zorder': 2},
+                 hist_kws={'alpha': 1, 'zorder': 1})
+    ax2: plt.Axes = ax.twinx()
+    sns.kdeplot(rri, color=utils.adjust_color('med', 0.75), ax=ax2, lw=2.0, zorder=1)
+    ax2.tick_params(axis='y', right=False, labelright=False)
+    ax2.set_ylim(0)
+
+    ax.set_title("Distribution of RR Intervals")
+    ax.set_xlabel("RR Intervals [ms]")
+    ax.set_ylabel("Count")
+
+    ax2.boxplot(
+        rri,
+        vert=False,
+        positions=[ax2.get_ylim()[-1] / 10 + 0.05 * ax2.get_ylim()[-1]],
+        widths=ax2.get_ylim()[-1] / 10,
+        manage_ticks=False,
+        patch_artist=True,
+        boxprops=dict(linewidth=2.0, color=utils.fau_color('fau'), facecolor=utils.adjust_color('fau', 2.0)),
+        medianprops=dict(linewidth=2.0, color=utils.fau_color('fau')),
+        whiskerprops=dict(linewidth=2.0, color=utils.fau_color('fau')),
+        capprops=dict(linewidth=2.0, color=utils.fau_color('fau')),
+        zorder=4
+    )
+
+    ax.set_xlim(0.95 * np.min(rri), 1.05 * np.max(rri))
+
+    return ax
