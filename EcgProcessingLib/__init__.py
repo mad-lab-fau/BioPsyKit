db60f02631d2f0c9c720a94bf76c5e929d42a042
Robert Richer <robert.richer@fau.de>
2020-08-17 13:59:02 +0200

    added top level documentation

--- EcgProcessingLib/__init__.py
+++ EcgProcessingLib/__init__.py
@@ -1,5 +1,16 @@
+"""
+Top level package for EcgProcessingLib.
+"""
+
 from .ecg_processor import *
 from .signal import *
 from .utils import *
 from .mist import *
 from .plotting import *
+
+# Info
+__version__ = "0.0.1"
+
+# Maintainer info
+__author__ = "Robert Richer"
+__email__ = "robert.richer@fau.de"
