2565201cffd68a26b24906f04f6afe5d4d3a7971
Robert Richer <robert.richer@fau.de>
2020-08-19 12:04:05 +0200

    - improved 'utils.split_data' to now also support pandas dataframes - added further util functions for data loading and exporting

--- EcgProcessingLib/utils.py
+++ EcgProcessingLib/utils.py
@@ -50,7 +50,7 @@ def adjust_color(key: str, amount: Optional[float] = 1.5) -> str:
     return mc.to_hex(colorsys.hls_to_rgb(c[0], max(0, min(1, amount * c[1])), c[2]))
 
 
-def split_data(time_intervals: Union[pd.Series, Dict[str, Sequence[str]]],
+def split_data(time_intervals: Union[pd.DataFrame, pd.Series, Dict[str, Sequence[str]]],
                dataset: Optional[Dataset] = None, df: Optional[pd.DataFrame] = None,
                timezone: Optional[Union[str, pytz.timezone]] = tz) -> Dict[str, pd.DataFrame]:
     """
@@ -58,11 +58,12 @@ def split_data(time_intervals: Union[pd.Series, Dict[str, Sequence[str]]],
 
     Parameters
     ----------
-    time_intervals : dict or pd.Series
+    time_intervals : dict or pd.Series or pd.DataFrame
         time intervals indicating where the data should be split.
-        Can either be a pandas Series with the `start` times of the single phases
-        (the names of the phases are then derived from the index) or a dictionary with tuples indicating
-        start and end times of the phases (the names of the phases are then derived from the dict keys)
+        Can either be a pandas Series or 1 row of a pandas Dataframe with the `start` times of the single phases
+        (the names of the phases are then derived from the index in case of a Series or column names in case of a
+        Dataframe) or a dictionary with tuples indicating start and end times of the phases
+        (the names of the phases are then derived from the dict keys)
     dataset : Dataset, optional
         NilsPodLib dataset object to be split
     df : pd.DataFrame, optional
@@ -102,6 +103,10 @@ def split_data(time_intervals: Union[pd.Series, Dict[str, Sequence[str]]],
             # convert to pytz object
             timezone = pytz.timezone(timezone)
         df = dataset.data_as_df("ecg", index="utc_datetime").tz_localize(tz=utc).tz_convert(tz=timezone)
+    if isinstance(time_intervals, pd.DataFrame):
+        if len(time_intervals) > 1:
+            raise ValueError("Only dataframes with 1 row allowed!")
+        time_intervals = time_intervals.iloc[0]
     if isinstance(time_intervals, pd.Series):
         for name, start, end in zip(time_intervals.index, np.pad(time_intervals, (0, 1)), time_intervals[1:]):
             data_dict[name] = df.between_time(start, end)
@@ -146,6 +151,46 @@ def check_input(ecg_processor: 'EcgProcessor', key: str, ecg_signal: pd.DataFram
     return True
 
 
+def read_time_log(file_path: path_t, index_cols: Optional[Union[str, Sequence[str]]] = None,
+                  phase_cols: Optional[Sequence[str]] = None) -> pd.DataFrame:
+    """
+    Loads time log file.
+
+    Parameters
+    ----------
+    file_path : str or path
+        path to time log file, either Excel or csv file
+    index_cols : str list, optional
+        column name (or list of column names) that should be used for dataframe index or ``None`` for no index.
+        Default: ``None``
+    phase_cols : list, optional
+        list of column names that contain time log information or ``None`` to use all columns. Default: ``None``
+
+    Returns
+    -------
+    pd.DataFrame
+        pandas dataframe with time log information
+
+    """
+    # ensure pathlib
+    file_path = Path(file_path)
+    if file_path.suffix in ['.xls', '.xlsx']:
+        df_time_log = pd.read_excel(file_path)
+    elif file_path.suffix in ['.csv']:
+        df_time_log = pd.read_csv(file_path)
+    else:
+        raise ValueError("Unrecognized file format {}!".format(file_path.suffix))
+
+    if isinstance(index_cols, str):
+        index_cols = [index_cols]
+
+    if index_cols:
+        df_time_log.set_index(index_cols, inplace=True)
+    if phase_cols:
+        df_time_log = df_time_log.loc[:, phase_cols]
+    return df_time_log
+
+
 def write_hr_to_excel(ecg_processor: 'EcgProcessor', file_path: path_t) -> None:
     """
     Writes heart rate dictionary of one subject to an Excel file.
@@ -164,13 +209,32 @@ def write_hr_to_excel(ecg_processor: 'EcgProcessor', file_path: path_t) -> None:
         path to file
 
     """
-    # ensure pathlib
-    file_path = Path(file_path)
+
+    write_dict_to_excel(ecg_processor.heart_rate, file_path)
+
+
+def write_dict_to_excel(data_dict: Dict[str, pd.DataFrame], file_path: path_t,
+                        index_col: Optional[bool] = True) -> None:
+    """
+    Writes a dictionary containing pandas dataframes to an Excel file.
+
+    Parameters
+    ----------
+    data_dict : dict
+        dict with pandas dataframes
+    file_path : str or path
+        filepath
+    index_col : bool, optional
+        ``True`` to include dataframe index in Excel export, ``False`` otherwise. Default: ``True``
+    """
 
     writer = pd.ExcelWriter(file_path, engine='xlsxwriter')
-    for label, df_hr in ecg_processor.heart_rate.items():
-        # un-localize because Excel doesn't support timezone-aware dates
-        df_hr.tz_localize(None).to_excel(writer, sheet_name=label)
+    for key in data_dict:
+        if isinstance(data_dict[key].index, pd.DatetimeIndex):
+            # un-localize DateTimeIndex because Excel doesn't support timezone-aware dates
+            data_dict[key].tz_localize(None).to_excel(writer, sheet_name=key, index=index_col)
+        else:
+            data_dict[key].to_excel(writer, sheet_name=key, index=index_col)
     writer.save()
 
 
@@ -178,7 +242,7 @@ def load_hr_excel(file_path: path_t) -> Dict[str, pd.DataFrame]:
     """
     Loads excel file containing heart rate data of one subject (as exported by `write_hr_to_excel`).
 
-    The dictionary will have the following format: { "<Sheet_Name>" : hr_dataframe }
+    The dictionary will have the following format: { "<Phase>" : hr_dataframe }
 
     Each hr_dataframe has the following format:
         * 'date' Index: DateTimeIndex with timestamps of the heart rate samples
@@ -199,6 +263,86 @@ def load_hr_excel(file_path: path_t) -> Dict[str, pd.DataFrame]:
     # (re-)localize each sheet since Excel does not support timezone-aware dates
     dict_hr = {k: v.tz_localize(tz) for k, v in dict_hr.items()}
     return dict_hr
+
+
+def load_hr_excel_all_subjects(base_path: path_t, subject_folder_pattern: str,
+                               filename_pattern: str) -> Dict[str, Dict[str, pd.DataFrame]]:
+    """
+    Loads HR processing results (as exported by `write_hr_to_excel`) from all subjects and combines them into one
+    dictionary ('HR subject dict').
+
+    The dictionary will have the following format:
+
+    { "<Subject_ID>" : {"<Phase>" : hr_dataframe } }@
+
+
+    Parameters
+    ----------
+    base_path : str or path
+        path to top-level folder containing all subject folders
+    subject_folder_pattern : str
+        subject folder pattern. Folder names are assumed to be Subject IDs
+    filename_pattern : str
+        filename pattern of HR result files
+
+    Returns
+    -------
+
+    Examples
+    --------
+    >>> import EcgProcessingLib as ep
+    >>> base_path = "./"
+    >>> dict_hr_subjects = ep.load_hr_excel_all_subjects(
+    >>>                         base_path, subject_folder_pattern="Vp_*",
+    >>>                         filename_pattern="ecg_result*.xlsx")
+    >>>
+    >>> print(dict_hr_subjects)
+    >>> {
+    >>>     'Vp_01': {}, # dict as returned by load_hr_excel
+    >>>     'Vp_02': {},
+    >>>     # ...
+    >>> }
+    """
+    subject_dirs = list(sorted(base_path.glob(subject_folder_pattern)))
+    dict_hr_subjects = {}
+    for subject_dir in subject_dirs:
+        subject_id = subject_dir.name
+        # check whether old processing results already exist
+        hr_files = sorted(subject_dir.glob(filename_pattern))
+        if len(hr_files) == 1:
+            dict_hr_subjects[subject_id] = load_hr_excel(hr_files[0])
+        else:
+            print("No HR data for subject {}".format(subject_id))
+    return dict_hr_subjects
+
+
+def normalize_heart_rate(dict_hr_subjects: Dict[str, Dict[str, pd.DataFrame]],
+                         normalize_to: str) -> Dict[str, Dict[str, pd.DataFrame]]:
+    """
+    Normalizes heart rate per subject to the phase specified by `normalize_to`.
+
+    The result is the relative change of heart rate compared to the mean heart rate in the `normalize_to` phase.
+
+    Parameters
+    ----------
+    dict_hr_subjects : dict
+        dictionary with heart rate data of all subjects as returned by `load_hr_excel_all_subjects`
+    normalize_to : str
+        phase (i.e., dict key) of data to normalize all other data to
+
+    Returns
+    -------
+    dict
+        dictionary with normalized heart rate data per subject
+    """
+
+    dict_hr_subjects_norm = {}
+    for subject_id, dict_hr in dict_hr_subjects.items():
+        bl_mean = dict_hr[normalize_to].mean()
+        dict_hr_norm = {phase: (df_hr - bl_mean) / bl_mean * 100 for phase, df_hr in dict_hr.items()}
+        dict_hr_subjects_norm[subject_id] = dict_hr_norm
+
+    return dict_hr_subjects_norm
 
 
 def write_result_dict_to_csv(result_dict: Dict[str, pd.DataFrame], file_path: path_t,
