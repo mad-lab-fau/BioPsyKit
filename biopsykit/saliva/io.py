a941d012391ad641fa55f6c57ea6eb8e0c9bf2c9
Robert Richer <robert.richer@fau.de>
2020-11-20 17:06:38 +0100

    improved saliva import

--- biopsykit/saliva/io.py
+++ biopsykit/saliva/io.py
@@ -1,6 +1,6 @@
 import warnings
 from pathlib import Path
-from typing import Optional, Sequence
+from typing import Optional, Sequence, Dict
 
 import pandas as pd
 import numpy as np
@@ -25,23 +25,42 @@ def load_saliva_plate(file_path: path_t, sample_id_col: str, data_col: str, biom
     else:
         df_saliva = pd.read_excel(file_path, skiprows=2, sheet_name=sheet_name, usecols=[sample_id_col, data_col])
 
-    df_saliva[["subject", "sample"]] = df_saliva[sample_id_col].str.extract("Vp(\d+) S(\d)").astype(int)
+    df_saliva[["subject", "sample"]] = df_saliva[sample_id_col].str.extract("Vp(\d+) S(\d)")
+    df_saliva["sample"] = df_saliva["sample"].astype(int)
 
     df_saliva.drop(columns=[sample_id_col], inplace=True, errors='ignore')
     df_saliva.rename(columns={data_col: biomarker_type}, inplace=True)
     df_saliva.set_index(["subject", "sample"], inplace=True)
+
+    if condition_list is not None:
+        if isinstance(condition_list, Sequence):
+            # Add Condition as new index level
+            df_saliva = df_saliva.set_index(
+                condition_list.repeat(len(df_saliva.index.get_level_values('sample').unique())),
+                append=True).reorder_levels(["subject", "condition", "sample"])
+        elif isinstance(condition_list, Dict):
+            df_cond = pd.DataFrame(condition_list)
+            df_cond = df_cond.stack().reset_index(level=1).set_index('level_1').sort_values(0)
+            df_cond.index.name = 'condition'
+            df_saliva = df_saliva.set_index(
+                df_cond.index.repeat(len(df_saliva.index.get_level_values('sample').unique())),
+                append=True).reorder_levels(["subject", "condition", "sample"])
+        elif isinstance(condition_list, pd.DataFrame):
+            condition_list = condition_list.reset_index().set_index("condition")
+            df_saliva = df_saliva.set_index(
+                condition_list.index.repeat(len(df_saliva.index.get_level_values('sample').unique())),
+                append=True).reorder_levels(["subject", "condition", "sample"])
+
     # Subject Exclusion
     # TODO move subject exclusion to extra function
+    # TODO add warning to documentation that subject exclusion is performed *AFTER* assigning conditions
+    # TODO add warning if this fails (e.g. because condition list has already excluded subjects and we're trying to merge it)
     if excluded_subjects:
         try:
             df_saliva.drop(index=excluded_subjects, inplace=True)
         except KeyError:
             warnings.warn("Not all subjects of {} exist in the dataset!".format(excluded_subjects))
 
-    if condition_list:
-        # Add Condition as new index level
-        df_saliva = df_saliva.set_index(condition_list.repeat(len(df_saliva.index.get_level_values('sample').unique())),
-                                        append=True).reorder_levels(["condition", "subject", "sample"])
     num_subjects = len(df_saliva.index.get_level_values("subject").unique())
     if saliva_times:
         _check_saliva_times(len(df_saliva), num_subjects, saliva_times)
@@ -49,7 +68,7 @@ def load_saliva_plate(file_path: path_t, sample_id_col: str, data_col: str, biom
     return df_saliva
 
 
-def save_saliva_biopsykit(file_path: path_t, data: pd.DataFrame) -> None:
+def save_saliva(file_path: path_t, data: pd.DataFrame) -> None:
     if 'time' in data:
         # drop saliva times for export
         data.drop('time', axis=1, inplace=True)
@@ -58,12 +77,16 @@ def save_saliva_biopsykit(file_path: path_t, data: pd.DataFrame) -> None:
     data.to_csv(file_path)
 
 
-def load_saliva_biopsykit(
+def load_saliva(
         file_path: path_t,
         biomarker_type: str,
         subject_col: Optional[str] = "subject",
+        condition_col: Optional[str] = 'condition',
         saliva_times: Optional[Sequence[int]] = None) -> pd.DataFrame:
-    data = pd.read_csv(file_path, index_col=subject_col)
+    index_cols = [subject_col]
+    if condition_col:
+        index_cols.append(condition_col)
+    data = pd.read_csv(file_path, index_col=index_cols)
 
     num_subjects = len(data)
     data.columns = data.columns.astype(int)
