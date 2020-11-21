4f33039354b89ca087c4189bb15841b0ed28d925
Robert Richer <robert.richer@fau.de>
2020-11-21 01:52:16 +0100

    added MIST HR example

--- biopsykit/example_data.py
+++ biopsykit/example_data.py
@@ -1,16 +1,25 @@
 from pathlib import Path
-from typing import Sequence
+from typing import Sequence, Dict
 
-EXAMPLE_DATA_PATH = Path(__file__).parent.parent.joinpath("example_data")
+import pandas as pd
+
+_EXAMPLE_DATA_PATH = Path(__file__).parent.parent.joinpath("example_data")
+
+__all__ = [
+    'list_example_data',
+    'get_file_path',
+    'get_saliva_example',
+    'get_mist_hr_example'
+]
 
 
 def list_example_data() -> Sequence[str]:
-    file_list = [filename.name for filename in EXAMPLE_DATA_PATH.glob("*") if filename.suffix in ['.csv', '.xlsx']]
+    file_list = [filename.name for filename in _EXAMPLE_DATA_PATH.glob("*") if filename.suffix in ['.csv', '.xlsx']]
     return file_list
 
 
 def get_file_path(file_name: str) -> Path:
-    file_path = EXAMPLE_DATA_PATH.joinpath(file_name)
+    file_path = _EXAMPLE_DATA_PATH.joinpath(file_name)
     if file_path.is_file():
         # file exists
         return file_path
@@ -19,9 +28,10 @@ def get_file_path(file_name: str) -> Path:
 
 def get_saliva_example():
     from biopsykit.saliva.io import load_saliva
-    return load_saliva(EXAMPLE_DATA_PATH.joinpath("cortisol_sample.csv"))
-    pass
+    return load_saliva(_EXAMPLE_DATA_PATH.joinpath("cortisol_sample.csv"), biomarker_type='cortisol')
 
 
-def get_mist_hr_example():
-    pass
+def get_mist_hr_example() -> Dict[str, pd.DataFrame]:
+    from biopsykit.signals.ecg.io import load_hr_subject_dict
+    return load_hr_subject_dict(_EXAMPLE_DATA_PATH.joinpath("hr_sample_mist.xlsx"))
+
