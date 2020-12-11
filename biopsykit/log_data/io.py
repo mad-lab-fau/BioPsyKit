ca370674eacdf55902bfe3bc9cc4ef4d1dfe8893
Robert Richer <robert.richer@fau.de>
2020-12-11 12:33:21 +0100

    improvements to log_data.io

--- biopsykit/log_data/io.py
+++ biopsykit/log_data/io.py
@@ -1,5 +1,6 @@
 import re
 import json
+import warnings
 from pathlib import Path
 from typing import Optional, Dict, Union
 
@@ -21,7 +22,8 @@ def load_logs_all_subjects(path: path_t, has_subfolder: Optional[bool] = True,
     has_subfolder
     path : path or str
         path to folder containing logs
-    log_filename_pattern
+    log_filename_pattern : str, optional
+    return_df : bool, optional
 
     Returns
     -------
@@ -32,32 +34,43 @@ def load_logs_all_subjects(path: path_t, has_subfolder: Optional[bool] = True,
     # ensure pathlib
     path = Path(path)
 
-    if log_filename_pattern is None:
-        log_filename_pattern = LOG_FILENAME_PATTERN + ".csv"
-
     dict_log_files = {}
     if has_subfolder:
-        folder_list = [p for p in sorted(path.glob("*")) if p.is_dir()]
+        folder_list = [p for p in sorted(path.glob("*")) if p.is_dir() and not p.name.startswith('.')]
         for folder in tqdm(folder_list):
             subject_id = folder.name
             dict_log_files[subject_id] = load_log_one_subject(folder)
     else:
+        # first, look for available csv files
         file_list = [p for p in sorted(path.glob("*.csv"))]
-        for file in tqdm(file_list):
-            subject_id = re.search(log_filename_pattern, file.name).group(1)
-            df = pd.read_csv(file, sep=';')
-            df['time'] = pd.to_datetime(df['time'])
-            df.set_index('time', inplace=True)
-            dict_log_files[subject_id] = df
+        if len(file_list) > 0:
+            if log_filename_pattern is None:
+                log_filename_pattern = LOG_FILENAME_PATTERN + ".csv"
+            for file in tqdm(file_list):
+                subject_id = re.search(log_filename_pattern, file.name).group(1)
+                df = pd.read_csv(file, sep=';')
+                df['time'] = pd.to_datetime(df['time'])
+                df.set_index('time', inplace=True)
+                dict_log_files[subject_id] = df
+        else:
+            # fallback: look for zip files
+            file_list = [p for p in sorted(path.glob("*.zip"))]
+            if len(file_list) > 0:
+                if log_filename_pattern is None:
+                    log_filename_pattern = LOG_FILENAME_PATTERN + ".zip"
+                for file in tqdm(file_list):
+                    subject_id = re.search(log_filename_pattern, file.name).group(1)
+                    dict_log_files[subject_id] = load_log_one_subject(file)
 
     if return_df:
-        return pd.concat(dict_log_files)
+        df = pd.concat(dict_log_files, names=['subject_id'])
+        return df
     else:
         return dict_log_files
 
 
 def load_log_one_subject(path: path_t, log_filename_pattern: Optional[str] = None,
-                         overwrite_logs: Optional[bool] = False) -> pd.DataFrame:
+                         overwrite_logs_unzip: Optional[bool] = False) -> pd.DataFrame:
     """
 
     Parameters
@@ -65,7 +78,7 @@ def load_log_one_subject(path: path_t, log_filename_pattern: Optional[str] = Non
     path : path or str
         path to subject folder containing log files or path to log file
     log_filename_pattern : str, optional
-    overwrite_logs : bool, optional
+    overwrite_logs_unzip : bool, optional
 
     Returns
     -------
@@ -81,15 +94,22 @@ def load_log_one_subject(path: path_t, log_filename_pattern: Optional[str] = Non
     path = Path(path)
 
     if path.is_dir():
+        # TODO add error messages if no log files are found (e.g. wrong folder path etc.)
         return log_folder_to_dataframe(path)
     if path.is_file():
         if path.suffix == '.zip':
             with zipfile.ZipFile(path, 'r') as zip_ref:
                 export_folder = path.parent.joinpath(re.search(log_filename_pattern, path.name).group(1))
                 export_folder.mkdir(exist_ok=True)
-                if overwrite_logs or len(list(export_folder.glob("*"))) == 0:
+                if overwrite_logs_unzip or len(list(export_folder.glob("*"))) == 0:
                     zip_ref.extractall(export_folder)
                     # call recursively with newly exported folder
+                    return log_folder_to_dataframe(export_folder)
+                else:
+                    # folder not empty => inform user and load folder
+                    warnings.warn(
+                        "Folder {} already contains log files which will be loaded. "
+                        "Set `overwrite_logs_unzip = True` to overwrite log files.".format(export_folder.name))
                     return log_folder_to_dataframe(export_folder)
         if path.suffix == '.csv':
             return log_folder_to_dataframe(path)
@@ -108,23 +128,26 @@ def log_folder_to_dataframe(folder_path: path_t) -> pd.DataFrame:
     return df
 
 
-def export_log_dataframe(df: pd.DataFrame, subject_id: str, folder_path: path_t,
-                         overwrite: Optional[bool] = False, show_skipped: Optional[bool] = False) -> bool:
-    export_path = folder_path.joinpath("logs_{}.csv".format(subject_id))
+def save_log_data(log_data: Union[pd.DataFrame, 'LogData'], path: path_t, subject_id: Optional[str] = None,
+                  overwrite: Optional[bool] = False, show_skipped: Optional[bool] = False):
+    from biopsykit.log_data import LogData
 
+    if isinstance(log_data, pd.DataFrame):
+        if isinstance(log_data.index, pd.MultiIndex):
+            # dataframe has a multiindex => it's a combined dataframe for all subjects
+            log_data.to_csv(path, sep=';')
+            return
+        else:
+            log_data = LogData(log_data)
+
+    if subject_id is None:
+        subject_id = log_data.subject_id
+
+    export_path = path.joinpath("logs_{}.csv".format(subject_id))
     if not export_path.exists() or overwrite:
-        # print("--- Summary {} ---".format(subject_id))
-        # log_data = LogData(df, error_handling='warn')
-        # print("Logged Days: {}".format([str(date) for date in log_data.log_days]))
-        # print("Finished Days: {}".format(log_data.num_finished_days))
-        # print("---------------------")
-        # log_data.export_csv(export_path)
-        df.to_csv(export_path, sep=";")
-        return True
+        log_data.df.to_csv(export_path, sep=";")
     elif show_skipped:
         print("Skipping {}. Already exported.".format(subject_id))
-
-    return False
 
 
 def _parse_date(row: pd.Series) -> pd.Series:
