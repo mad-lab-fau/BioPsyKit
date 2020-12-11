676ff6a80a9e0278467526f4d2703911407bb5d9
Robert Richer <robert.richer@fau.de>
2020-12-11 12:34:02 +0100

    improved Log Data widgets

--- biopsykit/log_data/widgets.py
+++ biopsykit/log_data/widgets.py
@@ -11,19 +11,27 @@ from biopsykit.log_data import LogData
 LOG_FILENAME_PATTERN = "logs_(.*?)"
 
 
-def log_file_subject_dropdown(path: path_t, value_type: Optional[str] = 'file_path',
+def log_file_subject_dropdown(path: path_t, input_type: Optional[str] = 'file', value_type: Optional[str] = 'file_path',
                               callback: Optional[Callable] = None) -> ipywidgets.Dropdown:
     import re
 
+    input_types = ['folder', 'file']
     value_types = ['file_path', 'file_name', 'folder_name', 'subject_id']
+
+    if input_type not in input_types:
+        raise ValueError("Invalid input_type!")
 
     if value_type not in value_types:
         raise ValueError("Invalid value_type!")
 
     path = Path(path)
-    log_file_pattern = LOG_FILENAME_PATTERN + ".csv"
-    log_file_list = [log_file for log_file in list(sorted(path.glob("*.csv")))]
-    subject_list = [re.search(log_file_pattern, log_file.name).group(1) for log_file in log_file_list]
+    if input_type == 'file':
+        log_file_pattern = LOG_FILENAME_PATTERN + ".csv"
+        log_file_list = [log_file for log_file in list(sorted(path.glob("*.csv")))]
+        subject_list = [re.search(log_file_pattern, log_file.name).group(1) for log_file in log_file_list]
+    if input_type == 'folder':
+        log_file_list = [folder for folder in path.glob("*") if folder.is_dir()]
+        subject_list = [folder.name for folder in log_file_list]
 
     option_list = [("Select Subject", None)]
     if value_type == 'file_path':
