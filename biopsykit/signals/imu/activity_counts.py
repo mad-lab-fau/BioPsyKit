bb7d4a2978a970fcfa100e75e8ed50f130692e89
Robert Richer <robert.richer@fau.de>
2020-12-14 10:27:12 +0100

    added further functions to activity_counts

--- biopsykit/signals/imu/activity_counts.py
+++ biopsykit/signals/imu/activity_counts.py
@@ -1,8 +1,11 @@
 from typing import Union
 
+import pandas as pd
 import numpy as np
 from scipy import signal
 from scipy import interpolate
+
+from biopsykit.utils import sanitize_input_nd, tz
 
 
 class ActivityCounts:
@@ -19,8 +22,11 @@ class ActivityCounts:
 
     data = None
     sampling_rate = None
-
     activity_counts_ = None
+
+    @staticmethod
+    def _compute_norm(data: np.ndarray) -> np.ndarray:
+        return np.linalg.norm(data, axis=1)
 
     @staticmethod
     def _aliasing_filter(data: np.ndarray, sampling_rate: Union[int, float]) -> np.ndarray:
@@ -74,8 +80,21 @@ class ActivityCounts:
         padded_data = np.pad(data, (0, n_samples - len(data) % n_samples), 'constant', constant_values=0)
         return padded_data.reshape((len(padded_data) // n_samples, -1)).mean(axis=1)
 
-    def calculate(self, data: np.ndarray, sampling_rate: Union[int, float]) -> 'ActivityCounts':
+    def calculate(self, data: Union[np.ndarray, pd.DataFrame], sampling_rate: Union[int, float]) -> Union[
+        np.ndarray, pd.DataFrame]:
+        start_time = None
+        if isinstance(data, pd.DataFrame):
+            data = data.filter(like='acc')
+            start_time = data.index[0]
+
         data = data.copy()
+        data = sanitize_input_nd(data)
+
+        if data.shape[1] not in (1, 3):
+            raise ValueError("{} takes only 1D or 3D accelerometer data! Got {}D data.".format(self.__class__.__name__,
+                                                                                               data.shape[1]))
+        if data.shape[1] != 1:
+            data = self._compute_norm(data)
 
         data = self._downsample(data, sampling_rate, 30)
         data = self._aliasing_filter(data, 30)
@@ -84,5 +103,12 @@ class ActivityCounts:
         data = np.abs(data)
         data = self._truncate(data)
         data = self._digitize_8bit(data)
-        self.activity_counts_ = self._accumulate_minute_bins(data)
-        return self
+        data = self._accumulate_minute_bins(data)
+
+        if start_time is not None:
+            # input was dataframe
+            data = pd.DataFrame(data, columns=['activity_counts'])
+            start_time = float(start_time.to_datetime64()) / 1e9
+            data.index = pd.to_datetime((data.index * 60 + start_time).astype(int), utc=True, unit='s').tz_convert(tz)
+
+        return data
