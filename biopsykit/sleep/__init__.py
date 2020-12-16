c47293c48cde4944576d330ce18296e806f55e25
Robert Richer <robert.richer@fau.de>
2020-12-16 22:21:20 +0100

    added sleep endpoint computation

--- biopsykit/sleep/__init__.py
+++ biopsykit/sleep/__init__.py
@@ -1,7 +1,11 @@
 import biopsykit.sleep.io
 import biopsykit.sleep.utils
+import biopsykit.sleep.sleep_endpoints
+
+from biopsykit.sleep.imu import *
 
 __all__ = [
     'io',
     'utils',
+    'sleep_endpoints',
 ]
