4284e05f98db85232aee2ef45463b4a91da701b8
Robert Richer <robert.richer@fau.de>
2020-12-18 12:54:00 +0100

    refactored sleep_imu_plot

--- biopsykit/sleep/plotting.py
+++ biopsykit/sleep/plotting.py
@@ -3,7 +3,10 @@ from typing import Union, Optional, Dict, Tuple
 import pandas as pd
 import numpy as np
 import seaborn as sns
+
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.transforms as mtrans
 
 import biopsykit.colors as colors
 
@@ -18,9 +21,7 @@ def sleep_imu_plot(data: pd.DataFrame,
                    downsample_factor: Optional[float] = 1,
                    ax: Optional[plt.Axes] = None,
                    figsize: Optional[Tuple[float, float]] = None) -> Union[Tuple[plt.Figure, plt.Axes], None]:
-    import matplotlib.dates as mdates
     import matplotlib.ticker as mticks
-    import matplotlib.transforms as mtrans
 
     fig: Union[plt.Figure, None] = None
     sns.set_palette(colors.cmap_fau_blue('3'))
@@ -55,117 +56,23 @@ def sleep_imu_plot(data: pd.DataFrame,
 
     data.plot(ax=ax)
 
+    # 00:00 (12 am) vline (if present)
+    if date == mrp_start.normalize():
+        ax.vlines([date + pd.Timedelta("1d")], 0, 1, transform=ax.get_xaxis_transform(),
+                  linewidths=3, linestyles='dotted', colors=colors.fau_color('tech'), zorder=0)
+
     bbox = dict(
         fc=(1, 1, 1, plt.rcParams['legend.framealpha']),
         ec=plt.rcParams['legend.edgecolor'],
         boxstyle="round",
     )
 
-    # Sleep Onset vline
-    ax.vlines([sleep_onset], 0, 1, transform=ax.get_xaxis_transform(), linewidth=3, linestyles='--',
-              colors=colors.fau_color('nat'), zorder=3)
-    # MRP Start vline
-    ax.vlines([mrp_start], 0, 1, transform=ax.get_xaxis_transform(), linewidth=3, linestyles='--',
-              colors=colors.fau_color('med'), zorder=3)
+    _plot_sleep_onset(sleep_onset, bbox, ax)
+    _plot_wake_onset(wake_onset, bbox, ax)
+    _plot_mrp_start(sleep_onset, mrp_start, bbox, ax)
+    _plot_mrp_end(wake_onset, mrp_end, bbox, ax)
 
-    # Sleep Onset Text + Arrow
-    ax.annotate(
-        "Sleep Onset",
-        xy=(mdates.date2num(sleep_onset), 0.90),
-        xycoords=mtrans.blended_transform_factory(ax.transData, ax.transAxes),
-        xytext=(mdates.date2num(sleep_onset + pd.Timedelta("20M")), 0.90),
-        textcoords=mtrans.blended_transform_factory(ax.transData, ax.transAxes),
-        ha='left',
-        va='center',
-        bbox=bbox,
-        size=14,
-        arrowprops=dict(
-            arrowstyle="->",
-            lw=2,
-            color=colors.fau_color('nat'),
-            shrinkA=0.0, shrinkB=0.0,
-        )
-    )
-
-    # MRP Start Text + Arrow
-    ax.annotate(
-        "MRP Start",
-        xy=(mdates.date2num(mrp_start), 0.80),
-        xycoords=mtrans.blended_transform_factory(ax.transData, ax.transAxes),
-        xytext=(mdates.date2num(sleep_onset + pd.Timedelta("20M")), 0.80),
-        textcoords=mtrans.blended_transform_factory(ax.transData, ax.transAxes),
-        ha='left',
-        va='center',
-        bbox=bbox,
-        size=14,
-        arrowprops=dict(
-            arrowstyle="->",
-            lw=2,
-            color=colors.fau_color('med'),
-            shrinkA=0.0, shrinkB=0.0,
-        )
-    )
-
-    # 00:00 (12 am) vline (if present)
-    if date == mrp_start.normalize():
-        ax.vlines([date + pd.Timedelta("1d")], 0, 1, transform=ax.get_xaxis_transform(),
-                  linewidths=3, linestyles='dotted', colors=colors.fau_color('tech'), zorder=0)
-
-    # Wake Onset vline
-    ax.vlines([wake_onset], 0, 1, transform=ax.get_xaxis_transform(), linewidth=3, linestyles='--',
-              colors=colors.fau_color('nat'), zorder=3)
-
-    # MRP End vline
-    ax.vlines([mrp_end], 0, 1, transform=ax.get_xaxis_transform(), linewidth=3, linestyles='--',
-              colors=colors.fau_color('med'), zorder=3)
-
-    # Wake Onset Text + Arrow
-    ax.annotate(
-        "Wake Onset",
-        xy=(mdates.date2num(wake_onset), 0.90),
-        xycoords=mtrans.blended_transform_factory(ax.transData, ax.transAxes),
-        xytext=(mdates.date2num(wake_onset - pd.Timedelta("20M")), 0.90),
-        textcoords=mtrans.blended_transform_factory(ax.transData, ax.transAxes),
-        ha='right',
-        va='center',
-        bbox=bbox,
-        size=14,
-        arrowprops=dict(
-            arrowstyle="->",
-            lw=2,
-            color=colors.fau_color('nat'),
-            shrinkA=0.0, shrinkB=0.0,
-        )
-    )
-
-    # Wake Onset Text + Arrow
-    ax.annotate(
-        "MRP End",
-        xy=(mdates.date2num(mrp_end), 0.80),
-        xycoords=mtrans.blended_transform_factory(ax.transData, ax.transAxes),
-        xytext=(mdates.date2num(wake_onset - pd.Timedelta("20M")), 0.80),
-        textcoords=mtrans.blended_transform_factory(ax.transData, ax.transAxes),
-        ha='right',
-        va='center',
-        bbox=bbox,
-        size=14,
-        arrowprops=dict(
-            arrowstyle="->",
-            lw=2,
-            color=colors.fau_color('med'),
-            shrinkA=0.0, shrinkB=0.0,
-        )
-    )
-
-    handles = {}
-    for (bout_name, bouts), bg_color, bg_alpha in zip({'sleep': sleep_bouts, 'wake': wake_bouts}.items(),
-                                                      sleep_imu_plot_params['background.color'],
-                                                      sleep_imu_plot_params['background.alpha']):
-        handle = None
-        for idx, bout in bouts.iterrows():
-            handle = ax.axvspan(bout['start'], bout['end'], color=bg_color, alpha=bg_alpha)
-
-        handles[bout_name] = handle
+    handles = _plot_sleep_wake_bouts(sleep_bouts, wake_bouts, ax)
 
     # wear_time['end'] = wear_time.index.shift(1, freq=pd.Timedelta("15M"))
     # wear_time = wear_time[wear_time['wear'] == 0.0]
@@ -196,3 +103,116 @@ def sleep_imu_plot(data: pd.DataFrame,
         fig.tight_layout()
         fig.autofmt_xdate(rotation=0, ha='center')
         return fig, ax
+
+
+def _plot_sleep_onset(sleep_onset, bbox: Dict, ax: plt.Axes):
+    # Sleep Onset vline
+    ax.vlines([sleep_onset], 0, 1, transform=ax.get_xaxis_transform(), linewidth=3, linestyles='--',
+              colors=colors.fau_color('nat'), zorder=3)
+
+    # Sleep Onset Text + Arrow
+    ax.annotate(
+        "Sleep Onset",
+        xy=(mdates.date2num(sleep_onset), 0.90),
+        xycoords=mtrans.blended_transform_factory(ax.transData, ax.transAxes),
+        xytext=(mdates.date2num(sleep_onset + pd.Timedelta("20M")), 0.90),
+        textcoords=mtrans.blended_transform_factory(ax.transData, ax.transAxes),
+        ha='left',
+        va='center',
+        bbox=bbox,
+        size=14,
+        arrowprops=dict(
+            arrowstyle="->",
+            lw=2,
+            color=colors.fau_color('nat'),
+            shrinkA=0.0, shrinkB=0.0,
+        )
+    )
+
+
+def _plot_wake_onset(wake_onset, bbox: Dict, ax: plt.Axes):
+    # Wake Onset vline
+    ax.vlines([wake_onset], 0, 1, transform=ax.get_xaxis_transform(), linewidth=3, linestyles='--',
+              colors=colors.fau_color('nat'), zorder=3)
+
+    # Wake Onset Text + Arrow
+    ax.annotate(
+        "Wake Onset",
+        xy=(mdates.date2num(wake_onset), 0.90),
+        xycoords=mtrans.blended_transform_factory(ax.transData, ax.transAxes),
+        xytext=(mdates.date2num(wake_onset - pd.Timedelta("20M")), 0.90),
+        textcoords=mtrans.blended_transform_factory(ax.transData, ax.transAxes),
+        ha='right',
+        va='center',
+        bbox=bbox,
+        size=14,
+        arrowprops=dict(
+            arrowstyle="->",
+            lw=2,
+            color=colors.fau_color('nat'),
+            shrinkA=0.0, shrinkB=0.0,
+        )
+    )
+
+
+def _plot_mrp_start(sleep_onset, mrp_start, bbox: Dict, ax: plt.Axes):
+    # MRP Start vline
+    ax.vlines([mrp_start], 0, 1, transform=ax.get_xaxis_transform(), linewidth=3, linestyles='--',
+              colors=colors.fau_color('med'), zorder=3)
+    # MRP Start Text + Arrow
+    ax.annotate(
+        "MRP Start",
+        xy=(mdates.date2num(mrp_start), 0.80),
+        xycoords=mtrans.blended_transform_factory(ax.transData, ax.transAxes),
+        xytext=(mdates.date2num(sleep_onset + pd.Timedelta("20M")), 0.80),
+        textcoords=mtrans.blended_transform_factory(ax.transData, ax.transAxes),
+        ha='left',
+        va='center',
+        bbox=bbox,
+        size=14,
+        arrowprops=dict(
+            arrowstyle="->",
+            lw=2,
+            color=colors.fau_color('med'),
+            shrinkA=0.0, shrinkB=0.0,
+        )
+    )
+
+
+def _plot_mrp_end(wake_onset, mrp_end, bbox: Dict, ax: plt.Axes):
+    # MRP End vline
+    ax.vlines([mrp_end], 0, 1, transform=ax.get_xaxis_transform(), linewidth=3, linestyles='--',
+              colors=colors.fau_color('med'), zorder=3)
+    # MRP End Text + Arrow
+    ax.annotate(
+        "MRP End",
+        xy=(mdates.date2num(mrp_end), 0.80),
+        xycoords=mtrans.blended_transform_factory(ax.transData, ax.transAxes),
+        xytext=(mdates.date2num(wake_onset - pd.Timedelta("20M")), 0.80),
+        textcoords=mtrans.blended_transform_factory(ax.transData, ax.transAxes),
+        ha='right',
+        va='center',
+        bbox=bbox,
+        size=14,
+        arrowprops=dict(
+            arrowstyle="->",
+            lw=2,
+            color=colors.fau_color('med'),
+            shrinkA=0.0, shrinkB=0.0,
+        )
+    )
+
+
+def _plot_sleep_wake_bouts(sleep_bouts: pd.DataFrame, wake_bouts: pd.DataFrame, ax: plt.Axes) -> Dict:
+    handles = {}
+    for (bout_name, bouts), bg_color, bg_alpha in zip({'sleep': sleep_bouts, 'wake': wake_bouts}.items(),
+                                                      sleep_imu_plot_params['background.color'],
+                                                      sleep_imu_plot_params['background.alpha']):
+        handle = None
+        for idx, bout in bouts.iterrows():
+            handle = ax.axvspan(bout['start'], bout['end'], color=bg_color, alpha=bg_alpha)
+
+        handles[bout_name] = handle
+
+    handles = {k: v for k, v in handles.items() if v is not None}
+    return handles
