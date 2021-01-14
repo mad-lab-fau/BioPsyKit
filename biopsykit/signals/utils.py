f339675f215a604c41eccb4b1dd763c2bbbd6be1
Robert Richer <robert.richer@fau.de>
2021-01-14 11:33:58 +0100

    (hopefully) finally fixed gaitmap import issue

--- biopsykit/signals/utils.py
+++ biopsykit/signals/utils.py
@@ -776,3 +776,50 @@ def sliding_window_view(arr: np.ndarray, window_length: int, overlap: int, nan_p
     view = np.squeeze(view)  # get rid of single-dimensional entries from the shape of an array.
 
     return view
+
+
+def _bool_fill(indices: np.ndarray, bool_values: np.ndarray, array: np.ndarray) -> np.ndarray:
+    """Fill a preallocated array with bool_values.
+
+    This method iterates over the indices and adds the values to the array at the given indices using a logical or.
+    """
+    for i in range(len(indices)):  # noqa: consider-using-enumerate
+        index = indices[i]
+        val = bool_values[i]
+        index = index[~np.isnan(index)]
+        # perform logical or operation to combine all overlapping window results
+        array[index] = np.logical_or(array[index], val)
+    return array
+
+
+def bool_array_to_start_end_array(bool_array: np.ndarray) -> np.ndarray:
+    """Find regions in bool array and convert those to start-end indices.
+
+    The end index is exclusive!
+
+    Parameters
+    ----------
+    bool_array : array with shape (n,)
+        boolean array with either 0/1, 0.0/1.0 or True/False elements
+
+    Returns
+    -------
+    array of [start, end] indices with shape (n,2)
+
+    Examples
+    --------
+    >>> example_array = np.array([0,0,1,1,0,0,1,1,1])
+    >>> start_end_list = bool_array_to_start_end_array(example_array)
+    >>> start_end_list
+    array([[2, 4],
+           [6, 9]])
+    >>> example_array[start_end_list[0, 0]: start_end_list[0, 1]]
+    array([1, 1])
+
+    """
+    # check if input is actually a boolean array
+    if not np.array_equal(bool_array, bool_array.astype(bool)):
+        raise ValueError("Input must be boolean array!")
+
+    slices = np.ma.flatnotmasked_contiguous(np.ma.masked_equal(bool_array, 0))
+    return np.array([[s.start, s.stop] for s in slices])
