0cd84be9c7f2fcba9ee5e7efc3a9ee4ab147a272
Robert Richer <robert.richer@fau.de>
2021-03-01 11:22:22 +0100

    refactored bp.example data

--- biopsykit/example_data.py
+++ biopsykit/example_data.py
@@ -37,13 +37,13 @@ def get_condition_list_example() -> pd.DataFrame:
 
 
 def get_saliva_example(saliva_times: Optional[Sequence[int]] = None) -> pd.DataFrame:
-    from biopsykit.saliva.io import load_saliva_wide_format
+    from biopsykit.io.saliva import load_saliva_wide_format
     return load_saliva_wide_format(_EXAMPLE_DATA_PATH.joinpath("cortisol_sample.csv"), biomarker_type='cortisol',
                                    saliva_times=saliva_times)
 
 
 def get_saliva_example_stroop(saliva_times: Optional[Sequence[int]] = None) -> pd.DataFrame:
-    from biopsykit.saliva.io import load_saliva_wide_format
+    from biopsykit.io.saliva import load_saliva_wide_format
     return load_saliva_wide_format(_EXAMPLE_DATA_PATH.joinpath("cortisol_sample_stroop.csv"), biomarker_type='cortisol',
                                    saliva_times=saliva_times)
 
@@ -53,34 +53,34 @@ def get_saliva_mean_se_example() -> Dict[str, pd.DataFrame]:
 
 
 def get_mist_hr_example() -> Dict[str, pd.DataFrame]:
-    from biopsykit.signals.ecg.io import load_hr_subject_dict
+    from biopsykit.io.ecg import load_hr_subject_dict
     return load_hr_subject_dict(_EXAMPLE_DATA_PATH.joinpath("hr_sample_mist.xlsx"))
 
 
 def get_ecg_example() -> Tuple[pd.DataFrame, int]:
-    from biopsykit.io import load_dataset_nilspod
+    from biopsykit.io.nilspod import load_dataset_nilspod
     return load_dataset_nilspod(file_path=_EXAMPLE_DATA_PATH.joinpath("ecg").joinpath("ecg_sample_Vp01.bin"),
                                 datastreams=['ecg'])
 
 
 def get_ecg_example_02() -> Tuple[pd.DataFrame, int]:
-    from biopsykit.io import load_dataset_nilspod
+    from biopsykit.io.nilspod import load_dataset_nilspod
     return load_dataset_nilspod(file_path=_EXAMPLE_DATA_PATH.joinpath("ecg").joinpath("ecg_sample_Vp02.bin"),
                                 datastreams=['ecg'])
 
 
 def get_sleep_analyzer_raw_example() -> pd.DataFrame:
-    from biopsykit.sleep.io import load_withings_sleep_analyzer_raw_folder
+    from biopsykit.io.sleep import load_withings_sleep_analyzer_raw_folder
     return load_withings_sleep_analyzer_raw_folder(_EXAMPLE_DATA_PATH.joinpath("sleep"))
 
 
 def get_sleep_analyzer_summary_example() -> pd.DataFrame:
-    from biopsykit.sleep.io import load_withings_sleep_analyzer_summary
+    from biopsykit.io.sleep import load_withings_sleep_analyzer_summary
     return load_withings_sleep_analyzer_summary(_EXAMPLE_DATA_PATH.joinpath("sleep").joinpath("sleep.csv"))
 
 
 def get_sleep_imu_example() -> Tuple[pd.DataFrame, int]:
-    from biopsykit.io import load_dataset_nilspod
+    from biopsykit.io.nilspod import load_dataset_nilspod
     try:
         return load_dataset_nilspod(
             file_path=_EXAMPLE_DATA_PATH.joinpath("sleep_imu").joinpath("sleep_imu_sample_01.bin"))
@@ -91,7 +91,7 @@ def get_sleep_imu_example() -> Tuple[pd.DataFrame, int]:
 
 
 def get_eeg_example() -> Tuple[pd.DataFrame, int]:
-    from biopsykit.signals.eeg.io import load_eeg_muse
+    from biopsykit.io.eeg import load_eeg_muse
 
     return load_eeg_muse(_EXAMPLE_DATA_PATH.joinpath("eeg_muse_example.csv"))
 
