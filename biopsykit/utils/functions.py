588e3c99959336ec45d9a875186fd059fc8b53e9
Robert Richer <robert.richer@fau.de>
2021-03-03 17:31:01 +0100

    updated example notebooks

--- biopsykit/utils/functions.py
+++ biopsykit/utils/functions.py
@@ -1,5 +1,12 @@
+from typing import Union
+
 import numpy as np
+import pandas as pd
 
 
-def se(d):
-    return np.std(d, ddof=1) / np.sqrt(len(d))
+def se(data: Union[np.ndarray, pd.Series]) -> Union[np.ndarray, pd.Series]:
+    return np.std(data, ddof=1) / np.sqrt(len(data))
+
+
+def mean_se(data: pd.DataFrame):
+    return data.agg([np.mean, se])
