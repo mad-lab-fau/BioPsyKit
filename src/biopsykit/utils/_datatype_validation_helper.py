cc48388f657fb2d83078e95cb89d9c394de3ec72
Robert Richer <robert.richer@fau.de>
2021-04-06 15:11:01 +0200

    datatype_validation_helper: improved doc and added more flexibility to check for multiindex levels (e.g., has at least the required number of levels, or has to have at least the required columns, or has to have the colors in order or not)

--- src/biopsykit/utils/_datatype_validation_helper.py
+++ src/biopsykit/utils/_datatype_validation_helper.py
@@ -11,6 +11,29 @@ from biopsykit.utils.exceptions import ValidationError, FileExtensionError
 def _assert_file_extension(
     file_name: path_t, expected_extension: Union[str, Sequence[str]], raise_exception: Optional[bool] = True
 ) -> Optional[bool]:
+    """Check if a file has the correct file extension.
+
+    Parameters
+    ----------
+    file_name : path or str
+        file name to check for correct extension
+    expected_extension : str or list of str
+        file extension (or a list of file extensions) to check for
+    raise_exception : bool, optional
+        whether to raise an exception or return a bool value
+
+    Returns
+    -------
+    ``True`` if ``file_name`` ends with one of the specified file extensions, ``False`` otherwise
+    (if ``raise_exception`` is ``False``)
+
+    Raises
+    ------
+    FileExtensionError
+        if ``raise_exception`` is ``True`` and ``file_name`` does not end with any of the specified
+        ``expected_extension``
+
+    """
     # ensure pathlib
     file_name = Path(file_name)
     if isinstance(expected_extension, str):
@@ -28,13 +51,13 @@ def _assert_file_extension(
 def _assert_is_dtype(
     obj, dtype: Union[type, Tuple[type, ...]], raise_exception: Optional[bool] = True
 ) -> Optional[bool]:
-    """Check if an object has a specific dtype.
+    """Check if an object has a specific data type.
 
     Parameters
     ----------
-    obj
+    obj : any object
         object to check
-    dtype
+    dtype : type or list of type
         data type of tuple of data types to check
     raise_exception : bool, optional
         whether to raise an exception or return a bool value
@@ -58,44 +81,11 @@ def _assert_is_dtype(
     return True
 
 
-def _assert_has_column_multiindex(
-    df: pd.DataFrame,
-    expected: Optional[bool] = True,
-    nlevels: Optional[int] = 2,
-    raise_exception: Optional[bool] = True,
-) -> Optional[bool]:
-    """Check if a :any:`pandas.DataFrame` has a :any:`pandas.MultiIndex` as columns.
-
-    Parameters
-    ----------
-    df : :class:`pandas.DataFrame`
-        The dataframe to check
-    expected : bool, optional
-        Whether the df is expected to have MultiIndex column or not
-    nlevels : int, optional
-        If MultiIndex is expected, how many levels the MultiIndex columns should have
-    raise_exception : bool, optional
-        whether to raise an exception or return a bool value
-
-    Returns
-    -------
-    ``True`` if ``df`` meets the expected column index format, ``False`` otherwise (if ``raise_exception`` is ``False``)
-
-    Raises
-    ------
-    ValidationError
-        if ``raise_exception` is ``True`` and ``df`` does not meet the expected column index format
-
-    """
-    return _multiindex_check_helper(
-        df=df, idx_or_col="column", expected=expected, nlevels=nlevels, raise_exception=raise_exception
-    )
-
-
 def _assert_has_multiindex(
     df: pd.DataFrame,
     expected: Optional[bool] = True,
     nlevels: Optional[int] = 2,
+    nlevels_atleast: Optional[int] = False,
     raise_exception: Optional[bool] = True,
 ) -> Optional[bool]:
     """Check if a :any:`pandas.DataFrame` has a :any:`pandas.MultiIndex` as index.
@@ -108,6 +98,9 @@ def _assert_has_multiindex(
         Whether the df is expected to have a MultiIndex index or not
     nlevels : int, optional
         If MultiIndex is expected, how many levels the MultiIndex index should have
+    nlevels_atleast : bool, optional
+        Whether the MultiIndex has to have at least ``nlevels`` (``True``)
+        or exactly match the number of levels (``False``)
     raise_exception : bool, optional
         whether to raise an exception or return a bool value
 
@@ -122,12 +115,21 @@ def _assert_has_multiindex(
 
     """
     return _multiindex_check_helper(
-        df=df, idx_or_col="index", expected=expected, nlevels=nlevels, raise_exception=raise_exception
+        df=df,
+        idx_or_col="index",
+        expected=expected,
+        nlevels=nlevels,
+        nlevels_atleast=nlevels_atleast,
+        raise_exception=raise_exception,
     )
 
 
 def _assert_has_index_levels(
-    df: pd.DataFrame, index_levels: Iterable[_Hashable], raise_exception: Optional[bool] = True
+    df: pd.DataFrame,
+    index_levels: Iterable[_Hashable],
+    match_atleast: Optional[bool] = False,
+    match_order: Optional[bool] = True,
+    raise_exception: Optional[bool] = True,
 ) -> Optional[bool]:
     """Check if the dataframe has all index level names.
 
@@ -137,6 +139,11 @@ def _assert_has_index_levels(
         The dataframe to check
     index_levels : list
         Set of index level names to check
+    match_atleast : bool, optional
+        Whether the MultiIndex columns have to have at least the specified column levels (``True``)
+        or exactly match the column levels (``False``)
+    match_order : bool, optional
+        Whether to also match the level order
     raise_exception : bool, optional
         whether to raise an exception or return a bool value
 
@@ -150,7 +157,14 @@ def _assert_has_index_levels(
         if ``raise_exception`` is ``True`` and ``df`` does not have the expected index level names
 
     """
-    return _multiindex_level_names_helper(df, index_levels, "index", raise_exception)
+    return _multiindex_level_names_helper(
+        df,
+        level_names=index_levels,
+        idx_or_col="index",
+        match_atleast=match_atleast,
+        match_order=match_order,
+        raise_exception=raise_exception,
+    )
 
 
 def _assert_has_columns(
@@ -203,8 +217,55 @@ def _assert_has_columns(
     return result
 
 
+def _assert_has_column_multiindex(
+    df: pd.DataFrame,
+    expected: Optional[bool] = True,
+    nlevels: Optional[int] = 2,
+    nlevels_atleast: Optional[int] = False,
+    raise_exception: Optional[bool] = True,
+) -> Optional[bool]:
+    """Check if a :any:`pandas.DataFrame` has a :any:`pandas.MultiIndex` as columns.
+
+    Parameters
+    ----------
+    df : :class:`pandas.DataFrame`
+        The dataframe to check
+    expected : bool, optional
+        Whether the df is expected to have MultiIndex column or not
+    nlevels : int, optional
+        If MultiIndex is expected, how many levels the MultiIndex columns should have
+    nlevels_atleast : bool, optional
+        Whether the MultiIndex has to have at least ``nlevels`` (``True``)
+        or exactly match the number of levels (``False``)
+    raise_exception : bool, optional
+        Whether to raise an exception or return a bool value
+
+    Returns
+    -------
+    ``True`` if ``df`` meets the expected column index format, ``False`` otherwise (if ``raise_exception`` is ``False``)
+
+    Raises
+    ------
+    ValidationError
+        if ``raise_exception` is ``True`` and ``df`` does not meet the expected column index format
+
+    """
+    return _multiindex_check_helper(
+        df=df,
+        idx_or_col="column",
+        expected=expected,
+        nlevels=nlevels,
+        nlevels_atleast=nlevels_atleast,
+        raise_exception=raise_exception,
+    )
+
+
 def _assert_has_columns_levels(
-    df: pd.DataFrame, column_levels: Iterable[_Hashable], raise_exception: Optional[bool] = True
+    df: pd.DataFrame,
+    column_levels: Iterable[_Hashable],
+    match_atleast: Optional[bool] = True,
+    match_order: Optional[bool] = True,
+    raise_exception: Optional[bool] = True,
 ) -> Optional[bool]:
     """Check if the dataframe has all column level names of a MultiIndex column.
 
@@ -214,8 +275,13 @@ def _assert_has_columns_levels(
         The dataframe to check
     column_levels : list
         Set of column level names to check
+    match_atleast : bool, optional
+        Whether the MultiIndex columns have to have at least the specified column levels (``True``)
+        or exactly match the column levels (``False``)
+    match_order : bool, optional
+        Whether to also match the level order
     raise_exception : bool, optional
-        whether to raise an exception or return a bool value
+        Whether to raise an exception or return a bool value
 
     Returns
     -------
@@ -227,18 +293,42 @@ def _assert_has_columns_levels(
         if ``raise_exception`` is ``True`` and ``df`` does not have the expected index level names
 
     """
-    return _multiindex_level_names_helper(df, column_levels, "column", raise_exception)
+    return _multiindex_level_names_helper(
+        df,
+        level_names=column_levels,
+        idx_or_col="column",
+        match_atleast=match_atleast,
+        match_order=match_order,
+        raise_exception=raise_exception,
+    )
 
 
 def _multiindex_level_names_helper(
-    df: pd.DataFrame, level_names: Iterable[_Hashable], idx_or_col: str, raise_exception: Optional[bool] = True
+    df: pd.DataFrame,
+    level_names: Iterable[_Hashable],
+    idx_or_col: str,
+    match_atleast: Optional[bool] = True,
+    match_order: Optional[bool] = True,
+    raise_exception: Optional[bool] = True,
 ) -> Optional[bool]:
     ex_levels = list(level_names)
     if idx_or_col == "index":
         ac_levels = list(df.index.names)
     else:
         ac_levels = list(df.columns.names)
-    if ex_levels != ac_levels:
+    if match_order:
+        if match_atleast:
+            ac_levels_slice = ac_levels[: len(ex_levels)]
+            expected = ex_levels == ac_levels_slice
+        else:
+            expected = ex_levels == ac_levels
+    else:
+        if match_atleast:
+            expected = all(level in ac_levels for level in ex_levels)
+        else:
+            expected = sorted(ex_levels) == sorted(ac_levels)
+
+    if not expected:
         if raise_exception:
             raise ValidationError(
                 "The dataframe is expected to have exactly the following {} level names ({}), "
@@ -253,6 +343,7 @@ def _multiindex_check_helper(
     idx_or_col: str,
     expected: Optional[bool] = True,
     nlevels: Optional[int] = 2,
+    nlevels_atleast: Optional[int] = False,
     raise_exception: Optional[bool] = True,
 ) -> Optional[bool]:
     if idx_or_col == "index":
@@ -263,7 +354,7 @@ def _multiindex_check_helper(
         nlevels_act = df.columns.nlevels
 
     if has_multiindex is not expected:
-        if expected is False:
+        if not expected:
             if raise_exception:
                 raise ValidationError(
                     "The dataframe is expected to have a single level as {0}. "
@@ -277,7 +368,11 @@ def _multiindex_check_helper(
             )
         return False
     if has_multiindex is True:
-        if not nlevels_act == nlevels:
+        if nlevels_atleast:
+            expected = nlevels_act >= nlevels
+        else:
+            expected = nlevels_act == nlevels
+        if not expected:
             if raise_exception:
                 raise ValidationError(
                     "The dataframe is expected to have a MultiIndex with {0} {1} levels. "
