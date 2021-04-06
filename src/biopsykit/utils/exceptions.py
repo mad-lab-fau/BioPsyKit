957c69958b89dad073690c23abfd794e476465e8
Robert Richer <robert.richer@fau.de>
2021-04-06 14:38:35 +0200

    added DataFrameTransformationError as new exception

--- src/biopsykit/utils/exceptions.py
+++ src/biopsykit/utils/exceptions.py
@@ -1,5 +1,7 @@
 """A set of custom exceptions."""
 
+__all__ = ["ValidationError", "FileExtensionError", "DataFrameTransformationError"]
+
 
 class ValidationError(Exception):
     """An error indicating that data-object does not comply with the guidelines."""
@@ -7,3 +9,7 @@ class ValidationError(Exception):
 
 class FileExtensionError(Exception):
     """An error indicating that the file name has the wrong file extension."""
+
+
+class DataFrameTransformationError(Exception):
+    """An error indicating that dataframe transformation failed."""
