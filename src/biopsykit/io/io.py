150fe14cbe2b858d7fe3ec6e61dc74903a9b14ea
Robert Richer <robert.richer@fau.de>
2021-04-19 14:59:51 +0200

    bp.io: docs, linting and tests for load_questionnaire_data and convert_time_log_datetime

--- src/biopsykit/io/io.py
+++ src/biopsykit/io/io.py
@@ -146,15 +146,12 @@ def load_time_log(
             sep="_",
             suffix="(start|end)",
         )
-        print(data)
 
         # ensure that "start" is always before "end"
         data = data.reindex(["start", "end"], level=-1)
         # unstack start|end level
         data = data.unstack()
 
-    print(data)
-    print(data.values.flatten())
     for val in data.values.flatten():
         _assert_is_dtype(val, str)
 
@@ -171,6 +168,8 @@ def load_subject_condition_list(
     """Load subject condition assignment from file.
 
     This function can be used to load a file that contains the assignment of subject IDs to study conditions.
+    It will return a dataframe or a dictionary that complies with BioPsyKit's naming convention. i.e.,
+    the subject ID index will be named ``subject`` and the condition column will be named ``condition``.
 
     Parameters
     ----------
@@ -194,9 +193,10 @@ def load_subject_condition_list(
 
     Returns
     -------
-        :class:`pandas.DataFrame` or dict
-        a pandas dataframe with subject IDs and condition assignments (if ``return_dict`` is ``False``) or
-        a dict with subject IDs per group (if ``return_dict`` is ``True``)
+    :class:`~biopsykit.datatype_helper.SubjectConditionDataFrame` or
+    :class:`~biopsykit.datatype_helper.SubjectConditionDict`
+        a standardized pandas dataframe with subject IDs and condition assignments (if ``return_dict`` is ``False``) or
+        a standardized dict with subject IDs per group (if ``return_dict`` is ``True``)
 
     Raises
     ------
@@ -247,29 +247,43 @@ def load_subject_condition_list(
 
 def load_questionnaire_data(
     file_path: path_t,
-    index_cols: Optional[Union[str, Sequence[str], Dict[str, str]]] = None,
-    remove_nan_rows: Optional[bool] = True,
+    subject_col: Optional[str] = None,
+    condition_col: Optional[str] = None,
+    additional_index_cols: Optional[Union[str, Sequence[str], Dict[str, str]]] = None,
     replace_missing_vals: Optional[bool] = True,
+    remove_nan_rows: Optional[bool] = True,
     sheet_name: Optional[Union[str, int]] = 0,
     **kwargs,
 ) -> pd.DataFrame:
     """Load questionnaire data from file.
 
+    The resulting dataframe will comply with BioPsyKit's naming conventions, i.e., the subject ID index will be
+    named ``subject`` and a potential condition index will be named ``condition``.
+
     Parameters
     ----------
     file_path : :any:`pathlib.path` or str
         path to time log file. Must either be an Excel or csv file
-    index_cols : str, list of str or dict, optional
-        specifies dataframe index. Can either be a string or a list strings to indicate column name(s) from the
-        dataframe that should be used as index level(s), or ``None`` for no index.
-        If the index levels of the time log dataframe should have different names than the columns in the file,
-        a dict specifying the mapping (column_name : new_index_name) can be passed. Default: ``None``
-    remove_nan_rows : bool, optional
-        ``True`` to remove rows that only contain NaN values (except the index cols), ``False`` to keep NaN rows.
-        Default: ``True``
+    subject_col : str, optional
+        name of column containing subject IDs. According to BioPsyKit's convention,
+        the subject ID column is expected to have the name ``subject``.
+        If the column name differs in the file has another name, the column will be renamed in the dataframe
+        returned by this function.
+    condition_col : str, optional
+        name of column containing condition assignments (if present). According to BioPsyKit's convention,
+        the condition column is expected to have the name ``condition``.
+        If the column name differs in the file has another name, the column will be renamed in the dataframe
+        returned by this function.
+    additional_index_cols : str, list of str, optional
+        additional index levels to be added to the dataframe.
+        Can either be a string or a list strings to indicate column name(s) that should be used as index level(s),
+        or ``None`` for no additional index levels. Default: ``None``
     replace_missing_vals : bool, optional
         ``True`` to replace encoded "missing values" from software like SPSS (e.g. -77, -99, or -66)
          to "actual" missing values (NaN). Default: ``True``
+    remove_nan_rows : bool, optional
+        ``True`` to remove rows that only contain NaN values (except the index cols), ``False`` to keep NaN rows.
+        Default: ``True``
     sheet_name : str or int, optional
         sheet_name identifier (str) or sheet_name index (int) if file is an Excel file.
         Default: 0 (i.e. first sheet in Excel file)
@@ -294,57 +308,82 @@ def load_questionnaire_data(
     else:
         data = pd.read_csv(file_path, **kwargs)
 
+    if subject_col is None:
+        subject_col = "subject"
+
+    _assert_has_columns(data, [[subject_col]])
+
+    if subject_col != "subject":
+        subject_col = {subject_col: "subject"}
+        data = data.rename(columns=subject_col)
+        subject_col = "subject"
+
+    index_cols = [subject_col]
+
+    if condition_col is not None:
+        _assert_has_columns(data, [[condition_col]])
+        condition_col = {condition_col: "condition"}
+        data = data.rename(columns=condition_col)
+        condition_col = "condition"
+        index_cols.append(condition_col)
+
+    if additional_index_cols is None:
+        additional_index_cols = []
+    if isinstance(additional_index_cols, str):
+        additional_index_cols = [additional_index_cols]
+
+    index_cols = index_cols + additional_index_cols
     data = _apply_index_cols(data, index_cols=index_cols)
 
-    if remove_nan_rows:
-        data = data.dropna(how="all")
     if replace_missing_vals:
         data = convert_nan(data)
+    if remove_nan_rows:
+        data = data.dropna(how="all")
     return data
 
 
-def load_stroop_inquisit_data(folder_path: path_t, cols: Optional[Sequence[str]] = None) -> Dict[str, pd.DataFrame]:
-    """Load Inquisit data collected during "Stroop Test".
-
-    Stroop Test reusults (mean response time, number of correct answers, etc.)
-    are exported per Stroop phase and are stored in a common folder. This function loads all exported `.iqdat` files,
-    transforms them into dataframes and combines them into a dictionary.
-
-    Parameters
-    ----------
-    folder_path : :any:`pathlib.Path` or str
-        path to the folder in which the Stroop test export files are stored
-    cols : list of str, optional
-        names of columns which should be imported and added to the dictionary
-
-    Returns
-    -------
-    dict
-        dictionary with Stroop Test parameters per Stroop Phase
-
-    """
-    dict_stroop = {}
-    # ensure pathlib
-    folder_path = Path(folder_path)
-    # look for all Inquisit files in the folder
-    dataset_list = list(sorted(folder_path.glob("*.iqdat")))
-    subject = ""
-    # iterate through data
-    for data_path in dataset_list:
-        df_stroop = pd.read_csv(data_path, sep="\t")
-        if subject != df_stroop["subject"][0]:
-            dict_stroop = {}
-        # set subject, stroop phase
-        subject = df_stroop["subject"][0]
-        subphase = "Stroop{}".format(str(df_stroop["sessionid"][0])[-1])
-        df_mean = df_stroop.mean(axis=0).to_frame().T
-
-        if cols:
-            dict_stroop[subphase] = df_mean[cols]
-        else:
-            dict_stroop[subphase] = df_mean
-
-    return dict_stroop
+# def load_stroop_inquisit_data(folder_path: path_t, cols: Optional[Sequence[str]] = None) -> Dict[str, pd.DataFrame]:
+#     """Load Inquisit data collected during "Stroop Test".
+#
+#     Stroop Test reusults (mean response time, number of correct answers, etc.)
+#     are exported per Stroop phase and are stored in a common folder. This function loads all exported `.iqdat` files,
+#     transforms them into dataframes and combines them into a dictionary.
+#
+#     Parameters
+#     ----------
+#     folder_path : :any:`pathlib.Path` or str
+#         path to the folder in which the Stroop test export files are stored
+#     cols : list of str, optional
+#         names of columns which should be imported and added to the dictionary
+#
+#     Returns
+#     -------
+#     dict
+#         dictionary with Stroop Test parameters per Stroop Phase
+#
+#     """
+#     dict_stroop = {}
+#     # ensure pathlib
+#     folder_path = Path(folder_path)
+#     # look for all Inquisit files in the folder
+#     dataset_list = list(sorted(folder_path.glob("*.iqdat")))
+#     subject = ""
+#     # iterate through data
+#     for data_path in dataset_list:
+#         df_stroop = pd.read_csv(data_path, sep="\t")
+#         if subject != df_stroop["subject"][0]:
+#             dict_stroop = {}
+#         # set subject, stroop phase
+#         subject = df_stroop["subject"][0]
+#         subphase = "Stroop{}".format(str(df_stroop["sessionid"][0])[-1])
+#         df_mean = df_stroop.mean(axis=0).to_frame().T
+#
+#         if cols:
+#             dict_stroop[subphase] = df_mean[cols]
+#         else:
+#             dict_stroop[subphase] = df_mean
+#
+#     return dict_stroop
 
 
 def convert_time_log_datetime(
@@ -352,26 +391,28 @@ def convert_time_log_datetime(
     dataset: Optional[Dataset] = None,
     df: Optional[pd.DataFrame] = None,
     date: Optional[Union[str, datetime.datetime]] = None,
-    timezone: Optional[Union[str, pytz.timezone]] = tz,
+    timezone: Optional[Union[str, pytz.timezone]] = None,
 ) -> pd.DataFrame:
     """Convert the time log information into datetime objects.
 
     This function converts time log information (containing only time, but no date)
     into datetime objects, thus, adds the `start date` of the recording. To specify the recording date,
-    either a NilsPod :class:`nilspodlib.Dataset` or a pandas dataframe with a :class:`pandas.DateTimeIndex`
+    either a NilsPod :class:`~nilspodlib.Dataset` or a pandas dataframe with a :class:`~pandas.DateTimeIndex`
     must be supplied from which the recording date can be extracted.
-    As an alternative, the date can be specified explicitly.
+    As an alternative, the date can be specified explicitly via ``date`` parameter.
 
     Parameters
     ----------
     time_log : pd.DataFrame
         pandas dataframe with time log information
-    dataset : :class:`nilspodlib.Dataset`, optional
+    dataset : :class:`~nilspodlib.Dataset`, optional
         NilsPod Dataset object extract time and date information. Default: ``None``
     df : :class:`pandas.DataFrame`, optional
         dataframe with :class:`pandas.DateTimeIndex` to extract time and date information. Default: ``None``
     date : str or datetime, optional
-        date used to convert time log information into datetime. Default: ``None``
+        datetime object or date string used to convert time log information into datetime.
+        If ``date`` is a string, it must be supplied in a common date format, e.g. "dd.mm.yyyy" or "dd/mm/yyyy".
+        Default: ``None``
     timezone : str or pytz.timezone, optional
         timezone of the acquired data to convert, either as string of as pytz object.
         Default: pytz.timezone('Europe/Berlin')
@@ -389,7 +430,10 @@ def convert_time_log_datetime(
 
     """
     if dataset is None and date is None and df is None:
-        raise ValueError("Either `dataset`, `df` or `date` must be supplied as argument!")
+        raise ValueError("Either `dataset`, `df` or `date` must be supplied as argument to retrieve date information!")
+
+    if timezone is None:
+        timezone = tz
 
     if dataset is not None:
         date = dataset.info.utc_datetime_start.date()
@@ -401,8 +445,17 @@ def convert_time_log_datetime(
             raise ValueError("Index of 'df' must be DatetimeIndex!")
     if isinstance(date, str):
         # ensure datetime
-        date = datetime.datetime(date)
-    time_log = time_log.applymap(lambda x: pytz.timezone(timezone).localize(datetime.datetime.combine(date, x)))
+        date = pd.to_datetime(date)
+        date = date.date()
+    if isinstance(timezone, str):
+        timezone = pytz.timezone(timezone)
+
+    if isinstance(time_log.values.flatten()[0], str):
+        # convert time strings into datetime.time object
+        time_log = time_log.applymap(pd.to_datetime)
+        time_log = time_log.applymap(lambda val: val.time())
+
+    time_log = time_log.applymap(lambda x: timezone.localize(datetime.datetime.combine(date, x)))
     return time_log
 
 
@@ -482,7 +535,7 @@ def write_result_dict(
 
 
 def _apply_index_cols(
-    data: pd.DataFrame, index_cols: Optional[Union[str, Sequence[str], Dict[str, str]]]
+    data: pd.DataFrame, index_cols: Optional[Union[str, Sequence[str], Dict[str, str]]] = None
 ) -> pd.DataFrame:
     new_index_cols = None
     if isinstance(index_cols, str):
@@ -491,11 +544,11 @@ def _apply_index_cols(
         new_index_cols = list(index_cols.values())
         index_cols = list(index_cols.keys())
 
-    if index_cols:
+    if index_cols is not None:
         _assert_has_columns(data, [index_cols])
         data = data.set_index(index_cols)
 
-    if new_index_cols:
+    if new_index_cols is not None:
         data.index = data.index.set_names(new_index_cols)
 
     return data
--- tests/test_io/test_io_io.py
+++ tests/test_io/test_io_io.py
@@ -5,11 +5,14 @@ import numpy as np
 import pandas as pd
 import pytest
 
-from biopsykit.io import load_subject_condition_list
+from biopsykit.io import load_subject_condition_list, load_questionnaire_data, convert_time_log_datetime
+from biopsykit.io.nilspod import load_dataset_nilspod
 from biopsykit.utils.exceptions import ValidationError
 
 from biopsykit.io.io import load_time_log
+from nilspodlib import Dataset
 from pandas._testing import assert_frame_equal, assert_index_equal
+from pytz import UnknownTimeZoneError
 
 TEST_FILE_PATH = Path("../data/test_files")
 
@@ -28,6 +31,10 @@ def time_log_no_index():
 
 def time_log_correct():
     df = pd.DataFrame(
+        [
+            ["12:32:05", "12:33:10", "12:35:00", "12:39:45", "12:42:00"],
+            ["12:54:50", "12:55:50", "12:57:55", "12:58:00", "13:02:10"],
+        ],
         columns=["Baseline", "Intervention", "Stress", "Recovery", "End"],
         index=pd.MultiIndex.from_tuples(
             [("Vp01", "Intervention"), ("Vp02", "Control")], names=["subject", "condition"]
@@ -38,6 +45,7 @@ def time_log_correct():
 
 def time_log_not_continuous_correct():
     df = pd.DataFrame(
+        [["12:32:05", "12:33:10", "12:35:00", "12:39:45"], ["12:54:50", "12:55:50", "12:57:55", "12:58:00"]],
         columns=pd.MultiIndex.from_product([["phase1", "phase2"], ["start", "end"]], names=[None, "time"]),
         index=pd.MultiIndex.from_tuples(
             [("Vp01", "Intervention"), ("Vp02", "Control")], names=["subject", "condition"]
@@ -56,6 +64,70 @@ def subject_condition_list_correct():
 
 def subject_condition_list_correct_dict():
     return {"Control": ["Vp02", "Vp03"], "Intervention": ["Vp01", "Vp04"]}
+
+
+def questionnaire_data_no_remove():
+    return pd.DataFrame(
+        {
+            "PSS_01": [4, 2, 3, -66, np.nan],
+            "PANAS_01": [2, 1, 4, -66, np.nan],
+        },
+        index=pd.MultiIndex.from_arrays(
+            [
+                ["Vp01", "Vp02", "Vp03", "Vp04", "Vp05"],
+                ["Control", "Intervention", "Intervention", "Control", "Control"],
+                ["pre", "pre", "post", "post", "pre"],
+            ],
+            names=["subject", "condition", "time"],
+        ),
+    )
+
+
+def questionnaire_data_replace_missing():
+    return pd.DataFrame(
+        {
+            "PSS_01": [4, 2, 3, np.nan, np.nan],
+            "PANAS_01": [2, 1, 4, np.nan, np.nan],
+        },
+        index=pd.MultiIndex.from_arrays(
+            [
+                ["Vp01", "Vp02", "Vp03", "Vp04", "Vp05"],
+                ["Control", "Intervention", "Intervention", "Control", "Control"],
+                ["pre", "pre", "post", "post", "pre"],
+            ],
+            names=["subject", "condition", "time"],
+        ),
+    )
+
+
+def questionnaire_data_remove_nan():
+    return pd.DataFrame(
+        {
+            "PSS_01": [4, 2, 3, -66],
+            "PANAS_01": [2, 1, 4, -66],
+        },
+        index=pd.MultiIndex.from_arrays(
+            [
+                ["Vp01", "Vp02", "Vp03", "Vp04"],
+                ["Control", "Intervention", "Intervention", "Control"],
+                ["pre", "pre", "post", "post"],
+            ],
+            names=["subject", "condition", "time"],
+        ),
+    )
+
+
+def questionnaire_data_replace_missing_remove_nan():
+    return pd.DataFrame(
+        {
+            "PSS_01": [4, 2, 3],
+            "PANAS_01": [2, 1, 4],
+        },
+        index=pd.MultiIndex.from_arrays(
+            [["Vp01", "Vp02", "Vp03"], ["Control", "Intervention", "Intervention"], ["pre", "pre", "post"]],
+            names=["subject", "condition", "time"],
+        ),
+    )
 
 
 class TestIoIo:
@@ -146,8 +218,7 @@ class TestIoIo:
             continuous_time=continuous_time,
             index_cols=["subject", "condition"],
         )
-        assert_index_equal(data_out.index, expected.index)
-        assert_index_equal(data_out.columns, expected.columns)
+        assert_frame_equal(data_out, expected)
 
     @pytest.mark.parametrize(
         "file_path, subject_col, condition_col, expected",
@@ -197,3 +268,87 @@ class TestIoIo:
             TEST_FILE_PATH.joinpath(file_path), subject_col=subject_col, condition_col=condition_col
         )
         assert_frame_equal(data_out, expected)
+
+    @pytest.mark.parametrize(
+        "file_path, subject_col, condition_col, additional_index_cols, expected",
+        [
+            ("questionnaire_data.xlsx", None, None, None, does_not_raise()),
+            ("questionnaire_data.csv", None, None, None, does_not_raise()),
+            ("questionnaire_data.csv", "subject", None, None, does_not_raise()),
+            ("questionnaire_data.csv", "subject", "condition", None, does_not_raise()),
+            ("questionnaire_data.csv", "subject", "condition", "time", does_not_raise()),
+            ("questionnaire_data.csv", "subject", "condition", ["time"], does_not_raise()),
+            ("questionnaire_data_other_column_names.csv", "ID", "Group", None, does_not_raise()),
+            ("questionnaire_data_other_column_names.csv", "ID", "condition", None, pytest.raises(ValidationError)),
+            ("questionnaire_data_other_column_names.csv", "subject", "Group", None, pytest.raises(ValidationError)),
+            ("questionnaire_data_other_column_names.csv", "ID", "Group", "test", pytest.raises(ValidationError)),
+            ("questionnaire_data_other_column_names.csv", "ID", "Group", "time", does_not_raise()),
+        ],
+    )
+    def test_load_questionnaire_data_raises(
+        self, file_path, subject_col, condition_col, additional_index_cols, expected
+    ):
+        with expected:
+            load_questionnaire_data(
+                TEST_FILE_PATH.joinpath(file_path),
+                subject_col=subject_col,
+                condition_col=condition_col,
+                additional_index_cols=additional_index_cols,
+            )
+
+    @pytest.mark.parametrize(
+        "file_path, replace_missing_vals, remove_nan_rows, expected",
+        [
+            ("questionnaire_data.csv", False, False, questionnaire_data_no_remove()),
+            ("questionnaire_data.csv", True, False, questionnaire_data_replace_missing()),
+            ("questionnaire_data.csv", False, True, questionnaire_data_remove_nan()),
+            ("questionnaire_data.csv", True, True, questionnaire_data_replace_missing_remove_nan()),
+            ("questionnaire_data.xlsx", False, False, questionnaire_data_no_remove()),
+            ("questionnaire_data.xlsx", True, False, questionnaire_data_replace_missing()),
+            ("questionnaire_data.xlsx", False, True, questionnaire_data_remove_nan()),
+            ("questionnaire_data.xlsx", True, True, questionnaire_data_replace_missing_remove_nan()),
+        ],
+    )
+    def test_load_questionnaire_data(self, file_path, replace_missing_vals, remove_nan_rows, expected):
+        data_out = load_questionnaire_data(
+            file_path=TEST_FILE_PATH.joinpath(file_path),
+            subject_col="subject",
+            condition_col="condition",
+            additional_index_cols="time",
+            replace_missing_vals=replace_missing_vals,
+            remove_nan_rows=remove_nan_rows,
+        )
+
+        assert_frame_equal(data_out, expected, check_dtype=False)
+
+    @pytest.mark.parametrize(
+        "time_log, dataset, df, date, timezone, expected",
+        [
+            (time_log_correct(), None, None, None, None, pytest.raises(ValueError)),
+            (
+                time_log_correct(),
+                Dataset.from_bin_file(TEST_FILE_PATH.joinpath("dataset_example.bin")),
+                None,
+                None,
+                None,
+                does_not_raise(),
+            ),
+            (time_log_correct(), None, pd.DataFrame(index=range(0, 10)), None, None, pytest.raises(ValueError)),
+            (
+                time_log_correct(),
+                None,
+                pd.DataFrame(index=pd.to_datetime(["00:01 12.02.2021", "00:02 12.02.2021", "00:03 12.02.2021"])),
+                None,
+                None,
+                does_not_raise(),
+            ),
+            (time_log_correct(), None, None, "hello", None, pytest.raises(ValueError)),
+            (time_log_correct(), None, None, "12.02.2021", None, does_not_raise()),
+            (time_log_correct(), None, None, "12.02.2021", None, does_not_raise()),
+            (time_log_correct(), None, None, "12/02/2021", "Europe/Berlin", does_not_raise()),
+            (time_log_correct(), None, None, "12/02/2021", "test", pytest.raises(UnknownTimeZoneError)),
+        ],
+    )
+    def test_convert_time_log_datetime_raises(self, time_log, dataset, df, date, timezone, expected):
+        with expected:
+            convert_time_log_datetime(time_log=time_log, dataset=dataset, df=df, date=date, timezone=timezone)
