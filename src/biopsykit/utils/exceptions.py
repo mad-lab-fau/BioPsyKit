974da99d1079fbd437300b352141477aaf71dd35
Robert Richer <robert.richer@fau.de>
2021-04-22 00:13:39 +0200

    added new exceptions: ValueRangeError, FeatureComputationError

--- src/biopsykit/utils/exceptions.py
+++ src/biopsykit/utils/exceptions.py
@@ -1,6 +1,12 @@
 """A set of custom exceptions."""
 
-__all__ = ["ValidationError", "FileExtensionError", "DataFrameTransformationError"]
+__all__ = [
+    "ValidationError",
+    "FileExtensionError",
+    "DataFrameTransformationError",
+    "ValueRangeError",
+    "FeatureComputationError",
+]
 
 
 class ValidationError(Exception):
@@ -13,3 +19,11 @@ class FileExtensionError(Exception):
 
 class DataFrameTransformationError(Exception):
     """An error indicating that dataframe transformation failed."""
+
+
+class ValueRangeError(Exception):
+    """An error indicating that scores are not in the expected range."""
+
+
+class FeatureComputationError(Exception):
+    """An error indicating that feature computation failed."""
