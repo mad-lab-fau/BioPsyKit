3a8f904a0e25bef15ca630e1aff6e31f0efe29e5
danielkrauss2 <daniel.k.krauss@fau.de>
2021-05-03 16:43:58 +0200

    sadeh with rescoring now file reader with method to import clened data

--- src/biopsykit/sleep/sleep_wake/file_reader.py
+++ src/biopsykit/sleep/sleep_wake/file_reader.py
@@ -24,15 +24,18 @@ def read_all_psg(file_path):
     :dict:
         dict that contains a pandas.DataFrame for every subject
     """
+    print("start reading psg-data")
     psg = {}
-    for i in range(20):  # try only with 20 datasets to lower memory consumption
+    for i in range(6812 ):
 
         try: #look if a dataset exists
             psg[i] = xml_reader(file_path + '\polysomnography/annotations-events-nsrr\mesa-sleep-' + "{:04d}".format(i) + '-nsrr.xml')
+            print('file '+str(i)+' readed in!')
+
         except:
             pass
             # print("{:04d}".format(i) + "not available")
-
+    print('Reading psg-data finished')
     return psg
 
 
@@ -75,13 +78,17 @@ def read_all_actigraphy(file_path):
         dict that contains a pandas.DataFrame for every subject
 
     """
+    print("Start reading Actigraphy-data!")
     actigraphy = {}
-    for i in range(20):  # try only with 20 datasets to lower memory consumption
+    for i in range(6812):
         try:
             actigraphy[i] = pd.read_csv(file_path + '/actigraphy\mesa-sleep-'+ "{:04d}".format(i) + '.csv')
+            print('file ' + str(i) + ' readed in!')
+
         except:
             pass
 
+    print("Reading Actigraphy data finished!")
     return actigraphy
 
 
@@ -104,8 +111,9 @@ def read_single_actigraphy(file_path,mesaid):
 
     try:
         actigraphy = pd.read_csv(file_path + '/actigraphy\mesa-sleep-'+ "{:04d}".format(mesaid) + '.csv')
-    except:
-        raise ImportError("Dataset don't exist")
+
+    except ImportError as e:
+        raise ImportError("Dataset don't exist") from e
 
 
     return actigraphy
@@ -129,13 +137,16 @@ def read_all_r_point(file_path):
         dict that contains a pandas.DataFrame for every subject
 
     """
+    print("Start reading r-point-data!")
     r_point = {}
-    for i in range(20):  # try only with 20 datasets to lower memory consumption
+    for i in range(6812):
         try:
             r_point[i] = pd.read_csv(file_path + '\polysomnography/annotations-rpoints\mesa-sleep-'+ "{:04d}".format(i) + '-rpoint.csv')
+            print('file ' + str(i) + ' readed in!')
         except:
             pass
 
+    print("Reading r-point-data finished!")
     return r_point
 
 
@@ -163,6 +174,21 @@ def xml_reader(file_path):
     df = pd.DataFrame.from_dict(data)   #build a dataframe with time and sleep/wake status
 
     return df
+
+
+def read_cleaned_data(file_path):
+    actigraphy = {}
+    psg = {}
+    for i in range(6812):
+        try:
+            actigraphy[i] = pd.read_csv(file_path + '/actigraphy\clean_actigraphy' + "{:04d}".format(i) + '.csv')
+            psg[i] = pd.read_csv(file_path + '/psg\clean_psg' + "{:04d}".format(i) + '.csv')
+            print('file ' + str(i) + ' readed in!')
+
+        except:
+            pass
+
+    return actigraphy, psg
 
 
 
