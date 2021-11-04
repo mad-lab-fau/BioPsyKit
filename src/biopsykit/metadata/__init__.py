fdaff14dbab799f5611649aa370fea7cd471e7e9
Robert Richer <robert.richer@fau.de>
2021-11-04 22:31:48 +0100

    0.3.3

--- src/biopsykit/metadata/__init__.py
+++ src/biopsykit/metadata/__init__.py
@@ -1,4 +1,4 @@
 """Module with functions to compute metadata information."""
-from biopsykit.metadata.metadata import bmi, whr, gender_counts
+from biopsykit.metadata.metadata import bmi, gender_counts, whr
 
 __all__ = ["bmi", "whr", "gender_counts"]
