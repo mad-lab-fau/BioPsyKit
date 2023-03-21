e1471826c3a09ca55fc2f5aed97e8e2267f03183
Robert Richer <robert.richer@fau.de>
2023-03-21 17:46:16 +0100

    added new module for computing additional classification metrics from SklearnPipelinePermuter

---
+++ src/biopsykit/classification/metrics/__init__.py
@@ -0,0 +1,4 @@
+"""Module for computing classification metrics."""
+from biopsykit.classification.metrics._metrics import compute_additional_metrics
+
+__all__ = ["compute_additional_metrics"]
---
+++ src/biopsykit/classification/metrics/_metrics.py
@@ -0,0 +1,62 @@
+"""Metrics for classification tasks."""
+from inspect import getmembers
+
+import numpy as np
+import pandas as pd
+import sklearn.metrics
+
+from biopsykit.utils._types import str_t
+
+
+def _apply_score(row: pd.Series, score_func, pos_label: str):
+    true_labels_folds = row[0]
+    predicted_labels_folds = row[1]
+    scores = [
+        score_func(true_labels, predicted_labels, pos_label=pos_label)
+        for true_labels, predicted_labels in zip(true_labels_folds, predicted_labels_folds)
+    ]
+    return pd.Series(scores)
+
+
+def compute_additional_metrics(metric_summary: pd.DataFrame, metrics: str_t, pos_label: str):
+    """Compute additional classification metrics from a ``SklearnPipelinePermuter`` metric summary dataframe.
+
+    Parameters
+    ----------
+    metric_summary : :class:`~pandas.DataFrame`
+        metric summary dataframe from :class:`~biopsykit.classification.model_selection.SklearnPipelinePermuter`
+    metrics : list
+        list of additional metrics to compute
+    pos_label : str
+        positive label
+
+    """
+    metric_slice = metric_summary[["true_labels_folds", "predicted_labels_folds"]].copy()
+    metric_out = {}
+
+    # ensure list
+    if isinstance(metrics, str):
+        metrics = [metrics]
+
+    for metric in metrics:
+        score_funcs = dict(getmembers(sklearn.metrics))
+        if metric in score_funcs:
+            score_func = score_funcs[f"{metric}"]
+        elif f"{metric}_score" in score_funcs:
+            score_func = score_funcs[f"{metric}_score"]
+            metric = f"{metric}_score"
+        else:
+            raise ValueError(f"Metric '{metric}' not found.")
+        metric_out[metric] = metric_slice.apply(_apply_score, args=(score_func, pos_label), axis=1)
+    metric_out = pd.concat(metric_out, names=["score", "folds"], axis=1)
+
+    metric_out = metric_out.stack(["score", "folds"])
+    metric_out = metric_out.groupby(metric_out.index.names[:-1]).agg(
+        [("mean", lambda x: np.mean), ("std", lambda x: np.std(x))]  # pylint:disable=unnecessary-lambda
+    )
+
+    metric_out = metric_out.unstack("score").sort_index(axis=1, level="score")
+    metric_out.columns = metric_out.columns.map("_test_".join)
+    metric_summary = metric_summary.join(metric_out)
+
+    return metric_summary
