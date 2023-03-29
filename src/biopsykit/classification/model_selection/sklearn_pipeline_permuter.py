f83b2f85c64d48d04ebe888cee57404148a8d23f
Robert Richer <robert.richer@fau.de>
2023-03-29 13:31:57 +0200

    SklearnPipelinePermuter: added method to compute additional metrics to classification results

--- src/biopsykit/classification/model_selection/sklearn_pipeline_permuter.py
+++ src/biopsykit/classification/model_selection/sklearn_pipeline_permuter.py
@@ -3,6 +3,7 @@ import functools
 import pickle
 import re
 from copy import deepcopy
+from inspect import getmembers
 from itertools import product
 from pathlib import Path
 from shutil import rmtree
@@ -10,6 +11,7 @@ from typing import Any, Dict, Optional, Sequence, Tuple, Union
 
 import numpy as np
 import pandas as pd
+import sklearn.metrics
 from joblib import Memory
 from sklearn.base import BaseEstimator, clone
 from sklearn.model_selection import BaseCrossValidator
@@ -780,3 +782,57 @@ class SklearnPipelinePermuter:
         permuter_copy.model_combinations = list(set(permuter_copy.model_combinations))
 
         return permuter_copy
+
+    @staticmethod
+    def _apply_score(row: pd.Series, score_func, pos_label: str):
+        true_labels_folds = row[0]
+        predicted_labels_folds = row[1]
+        scores = [
+            score_func(true_labels, predicted_labels, pos_label=pos_label)
+            for true_labels, predicted_labels in zip(true_labels_folds, predicted_labels_folds)
+        ]
+        return pd.Series(scores)
+
+    def compute_additional_metrics(self, metrics: str_t, pos_label: str):
+        """Compute additional classification metrics.
+
+        Parameters
+        ----------
+        metrics : str or list of str
+            metric(s) to compute
+        pos_label : str
+            positive label for binary classification
+
+        Returns
+        -------
+        :class:`~pandas.DataFrame`
+            metric summary with additional metrics computed
+
+        """
+        if isinstance(metrics, str):
+            metrics = [metrics]
+        metric_summary = self.metric_summary()
+        metric_slice = metric_summary[["true_labels_folds", "predicted_labels_folds"]].copy()
+        metric_out = {}
+        for metric in metrics:
+            score_funcs = dict(getmembers(sklearn.metrics))
+            if metric in score_funcs:
+                score_func = score_funcs[f"{metric}"]
+                metric = metric.replace("_score", "")
+            elif f"{metric}_score" in score_funcs:
+                score_func = score_funcs[f"{metric}_score"]
+            else:
+                raise ValueError(f"Metric '{metric}' not found.")
+            metric_out[metric] = metric_slice.apply(self._apply_score, args=(score_func, pos_label), axis=1)
+        metric_out = pd.concat(metric_out, names=["score", "folds"], axis=1)
+
+        metric_out = metric_out.stack(["score", "folds"])
+        metric_out = metric_out.groupby(metric_out.index.names[:-1]).agg(
+            [("mean", lambda x: np.mean(x)), ("std", lambda x: np.std(x))]
+        )
+
+        metric_out = metric_out.unstack("score").sort_index(axis=1, level="score")
+        metric_out.columns = metric_out.columns.map("_test_".join)
+        metric_summary = metric_summary.join(metric_out)
+
+        return metric_summary
