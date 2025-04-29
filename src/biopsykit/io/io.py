19142acfc03328761666f5fd4321886be1fc805f
Robert Richer <robert.richer@fau.de>
2025-04-29 16:30:52 +0200

    refactor timelog processing to handle dayfirst format in datetime conversion

--- src/biopsykit/io/io.py
+++ src/biopsykit/io/io.py
@@ -293,7 +293,11 @@ def load_atimelogger_file(file_path: path_t, timezone: Optional[Union[datetime.t
     timelog.index.name = "phase"
     timelog.columns.name = "start_end"
 
-    timelog = timelog.apply(pd.to_datetime, axis=1).map(lambda val: val.tz_localize(timezone))
+    probe_timelog = timelog.iloc[0, 0]
+    dayfirst = "/" in probe_timelog
+    timelog = timelog.apply(lambda ts, d=dayfirst: pd.to_datetime(ts, dayfirst=d), axis=1).map(
+        lambda val: val.tz_localize(timezone)
+    )
 
     timelog = pd.DataFrame(timelog.T.unstack(), columns=["time"])
     timelog = timelog[::-1].reindex(["start", "end"], level="start_end")
